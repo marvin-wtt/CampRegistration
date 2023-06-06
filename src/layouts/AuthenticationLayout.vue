c2b2bc03357fe18fdda9f949900306c8bb8a0054
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-06-06 14:15:20 +0200

    Remove auth background

--- src/layouts/AuthenticationLayout.vue
+++ src/layouts/AuthenticationLayout.vue
@@ -11,12 +11,6 @@
 <script lang="ts" setup></script>
 
 <style scoped>
-.q-page-container {
-  background: linear-gradient(45deg, #338d8e, #093b3b);
-  background-size: 200% 200%;
-  animation: gradient 10s ease infinite;
-}
-
 @keyframes gradient {
   0% {
     background-position: 0% 50%;
