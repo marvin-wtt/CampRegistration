b8465ea5ded8e0af940c83942f86954236c479a8
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-07-02 02:45:56 +0200

    Remove console log

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -41,8 +41,6 @@ const handleFileError = (req: Request) => {
 const validate =
   (schema: ValidationSchema) =>
   (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
-
     const validSchema = pick(schema, ["params", "query", "body"]);
     const obj = pick(req, Object.keys(validSchema) as (keyof Request)[]);
 
