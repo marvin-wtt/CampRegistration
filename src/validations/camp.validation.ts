dd58076b3c4e38db658dcdd317821213c183cd9e
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-07-02 02:44:51 +0200

    Convert camp update to patch request

--- src/validations/camp.validation.ts
+++ src/validations/camp.validation.ts
@@ -1,29 +1,6 @@
 import Joi from "joi";
 import { type Prisma } from "@prisma/client";
 
-const validateCampBody = Joi.object<Prisma.CampCreateInput>({
-  public: Joi.boolean(),
-  countries: Joi.array()
-    .items(Joi.string().lowercase().length(2))
-    .min(1)
-    .required(),
-  name: Joi.alternatives()
-    .try(Joi.string(), Joi.object().pattern(Joi.string(), Joi.string()))
-    .required(),
-  maxParticipants: Joi.alternatives()
-    .try(Joi.number(), Joi.object().pattern(Joi.string(), Joi.number()))
-    .required(),
-  startAt: Joi.date().iso().required(),
-  endAt: Joi.date().iso().min(Joi.ref("startAt")).required(),
-  minAge: Joi.number().integer().min(0).max(99).required(),
-  maxAge: Joi.number().integer().min(Joi.ref("minAge")).max(99).required(),
-  location: Joi.alternatives()
-    .try(Joi.string(), Joi.object().pattern(Joi.string(), Joi.string()))
-    .required(),
-  price: Joi.number().min(0).required(),
-  form: Joi.object().required(),
-});
-
 const show = {
   params: Joi.object({
     campId: Joi.string(),
@@ -48,14 +25,52 @@ const index = {
 };
 
 const store = {
-  body: validateCampBody,
+  body: Joi.object<Prisma.CampCreateInput>({
+    public: Joi.boolean().default(false),
+    countries: Joi.array()
+      .items(Joi.string().lowercase().length(2))
+      .min(1)
+      .required(),
+    name: Joi.alternatives()
+      .try(Joi.string(), Joi.object().pattern(Joi.string(), Joi.string()))
+      .required(),
+    maxParticipants: Joi.alternatives()
+      .try(Joi.number(), Joi.object().pattern(Joi.string(), Joi.number()))
+      .required(),
+    startAt: Joi.date().iso().required(),
+    endAt: Joi.date().iso().min(Joi.ref("startAt")).required(),
+    minAge: Joi.number().integer().min(0).max(99).required(),
+    maxAge: Joi.number().integer().min(Joi.ref("minAge")).max(99).required(),
+    location: Joi.alternatives()
+      .try(Joi.string(), Joi.object().pattern(Joi.string(), Joi.string()))
+      .required(),
+    price: Joi.number().min(0).required(),
+    form: Joi.object().required(),
+  }),
 };
 
 const update = {
   params: Joi.object({
     campId: Joi.string(),
   }),
-  body: validateCampBody,
+  body: Joi.object<Prisma.CampCreateInput>({
+    public: Joi.boolean(),
+    countries: Joi.array()
+      .items(Joi.string().lowercase().length(2))
+      .min(1),
+    name: Joi.alternatives()
+      .try(Joi.string(), Joi.object().pattern(Joi.string(), Joi.string())),
+    maxParticipants: Joi.alternatives()
+      .try(Joi.number(), Joi.object().pattern(Joi.string(), Joi.number())),
+    startAt: Joi.date().iso(),
+    endAt: Joi.date().iso().min(Joi.ref("startAt")),
+    minAge: Joi.number().integer().min(0).max(99),
+    maxAge: Joi.number().integer().min(Joi.ref("minAge")).max(99),
+    location: Joi.alternatives()
+      .try(Joi.string(), Joi.object().pattern(Joi.string(), Joi.string())),
+    price: Joi.number().min(0),
+    form: Joi.object(),
+  }).options({ stripUnknown: true }),
 };
 
 const destroy = {
