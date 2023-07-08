da8f0932d1b271c339448bf58d5bb4717cf4b04f
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-07-08 05:00:44 +0200

    Update password requirements

--- src/validations/custom.validation.ts
+++ src/validations/custom.validation.ts
@@ -2,6 +2,10 @@ import passwordComplexity from "joi-password-complexity";
 
 export const PasswordSchema =  passwordComplexity({
   min: 8,
+  max: 26,
+  lowerCase: 1,
+  upperCase: 1,
   symbol: 1,
   numeric: 1,
+  requirementCount: 3,
 });
