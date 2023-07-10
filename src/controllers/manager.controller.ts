3f439cd3b1cfa9428fdd1d857d1f42d530966802
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-07-10 04:47:51 +0200

    Wip manager controller

--- src/controllers/manager.controller.ts
+++ src/controllers/manager.controller.ts
@@ -2,18 +2,17 @@ import { catchRequestAsync } from "../utils/catchAsync";
 import ApiError from "../utils/ApiError";
 import httpStatus from "http-status";
 import { collection, resource } from "../resources/resource";
-import {
-  emailService,
-  managerService,
-  templateService,
-  userService,
-} from "../services";
-import { templateResource } from "../resources";
+import { emailService, managerService } from "../services";
+import { campManagerResource } from "../resources";
+import { routeModel } from "../utils/verifyModel";
 
 const index = catchRequestAsync(async (req, res) => {
   const { campId } = req.params;
-  // TODO
-  res.send(httpStatus.NOT_IMPLEMENTED);
+
+  const managers = await managerService.getManagers(campId);
+  const resources = managers.map((manager) => campManagerResource(manager));
+
+  res.json(collection(resources));
 });
 
 const store = catchRequestAsync(async (req, res) => {
@@ -36,28 +35,45 @@ const store = catchRequestAsync(async (req, res) => {
   const token = manager.invitation?.token;
   // TODO Send notification
 
-  res.status(201).send();
-
-  // TODO Generate invitation token
-  //  send email
-
-  res.send(httpStatus.NOT_IMPLEMENTED);
-});
-
-const acceptInvite = catchRequestAsync(async (req, res) => {
-  const { campId, managerId } = req.params;
-  // TODO
-  res.send(httpStatus.NOT_IMPLEMENTED);
+  res.status(201).json(resource(campManagerResource(manager)));
 });
 
 const destroy = catchRequestAsync(async (req, res) => {
   const { campId, managerId } = req.params;
-  // TODO
+
+  const managers = await managerService.getManagers(campId);
+  if (managers.length <= 1) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "The camp must always have at least one camp manager.");
+  }
+
+  await managerService.removeManager(managerId);
+
+  res.send(httpStatus.NO_CONTENT);
+});
+
+const accept = catchRequestAsync(async (req, res) => {
+  const { campId, managerId, token } = req.params;
+
+  const manager = routeModel(req.models.manager);
+
   res.send(httpStatus.NOT_IMPLEMENTED);
+  // TODO Get typed manager
+  // const invitation = manager.invitation as Invitation | null;
+  //
+  // if (!invitation || invitation.token !== token) {
+  //   throw new ApiError(httpStatus.FORBIDDEN, "Invalid invitation token");
+  // }
+  //
+  // const userId: string = req.user?.id;
+  //
+  // const updatedManager = await managerService.acceptManagerInvitation(managerId, userId);
+  //
+  // res.json(resource(campManagerResource(updatedManager)));
 });
 
 export default {
   index,
   store,
   destroy,
+  accept,
 };
