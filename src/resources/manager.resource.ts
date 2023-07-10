2ef1669916e3bd67ddb7aaba5262f7cca9956d6b
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-07-10 04:47:15 +0200

    Add manager recourse

---
+++ src/resources/manager.resource.ts
@@ -0,0 +1,25 @@
+import {CampManager, Invitation, User} from "@prisma/client";
+
+interface ManagerWithRelationships extends CampManager {
+  user: User | null;
+  invitation: Invitation | null;
+}
+
+const managerResource = (manager: ManagerWithRelationships) => {
+  const { user, invitation } = manager;
+
+  const name: string | null = user ? user.name : null;
+  const email: string = invitation ? invitation.email : user?.email ?? '';
+  const status: string = user ? 'accepted' : 'pending';
+  const role: string = 'manager';
+
+  return {
+    id: manager.id,
+    name,
+    email,
+    status,
+    role,
+  };
+};
+
+export default managerResource;
