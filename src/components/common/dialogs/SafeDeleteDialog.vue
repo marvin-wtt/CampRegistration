1b17d075d7ecbecc4e705318e2b40e8561c3d0c7
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-07-10 04:48:30 +0200

    Add safe delete dialog

---
+++ src/components/common/dialogs/SafeDeleteDialog.vue
@@ -0,0 +1,103 @@
+<template>
+  <q-dialog
+    ref="dialogRef"
+    persistent
+    @hide="onDialogHide"
+  >
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">
+          {{ props.title }}
+        </div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ props.message }}
+      </q-card-section>
+
+      <q-card-section class="q-pt-none text-center text-bold">
+        {{ props.value }}
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-input
+          v-model="modelValue"
+          :label="props.label"
+          autofocus
+          rounded
+          outlined
+          @keyup.enter="prompt = false"
+        />
+      </q-card-section>
+
+      <q-card-actions
+        align="right"
+        class="text-primary"
+      >
+        <q-btn
+          :label="t('action.cancel')"
+          flat
+          outline
+          rounded
+          @click="onDialogCancel"
+        />
+        <q-btn
+          :disable="confirmDeleteDisabled"
+          :label="t('action.delete')"
+          color="negative"
+          rounded
+          @click="onDialogOK"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts" setup>
+import { useDialogPluginComponent } from 'quasar';
+import { useI18n } from 'vue-i18n';
+import { computed, ref } from 'vue';
+
+const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
+  useDialogPluginComponent();
+const { t } = useI18n();
+
+interface Props {
+  title: string;
+  message: string;
+  value: string;
+  label: string;
+}
+
+const props = defineProps<Props>();
+defineEmits([...useDialogPluginComponent.emits]);
+
+const modelValue = ref<string>('');
+
+const confirmDeleteDisabled = computed<boolean>(() => {
+  return (
+    modelValue.value.trim().toLowerCase() !==
+    props.value.trim().toLocaleLowerCase()
+  );
+});
+</script>
+
+<style scoped></style>
+
+<i18n lang="yaml" locale="en">
+action:
+  delete: 'Delete'
+  cancel: 'Cancel'
+</i18n>
+
+<i18n lang="yaml" locale="de">
+action:
+  delete: 'Löschen'
+  cancel: 'Abbrechen'
+</i18n>
+
+<i18n lang="yaml" locale="fr">
+action:
+  delete: 'Supprimer'
+  cancel: 'Annuler'
+</i18n>
