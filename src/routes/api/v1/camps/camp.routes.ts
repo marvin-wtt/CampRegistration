ac275cbb4146b11fb3f8cd2d33a00152a6e1545d
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-07-10 04:47:40 +0200

    Add manager routes

--- src/routes/api/v1/camps/camp.routes.ts
+++ src/routes/api/v1/camps/camp.routes.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import { auth, guard, validate } from "../../../../middlewares";
 import { campManager, campPublic } from "../../../../guards";
-import { campController } from "../../../../controllers";
-import { campValidation } from "../../../../validations";
-import registrationRoutes from "./registration.routes";
-import templateRoutes from "./template.routes";
-import { campService } from "../../../../services";
 import { verifyModelExists } from "../../../../utils/verifyModel";
 import { catchParamAsync } from "../../../../utils/catchAsync";
+import { campController } from "../../../../controllers";
+import { campValidation } from "../../../../validations";
+import { campService } from "../../../../services";
+import registrationRoutes from "./registration.routes";
+import templateRoutes from "./template.routes";
 import roomRoutes from "./room.routes";
+import managerRoutes from "./manager.routes";
 
 const router = express.Router({ mergeParams: true });
 
@@ -23,6 +24,7 @@ router.param(
 
 router.use("/:campId/registrations", registrationRoutes);
 router.use("/:campId/templates", templateRoutes);
+router.use("/:campId/managers", managerRoutes);
 router.use("/:campId/rooms", roomRoutes);
 
 router.get("/", validate(campValidation.index), campController.index);
