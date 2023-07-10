cac49d16d1b6974215903c8abbacc96f9e80c9b1
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-07-10 04:47:34 +0200

    Add manager recourse

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -2,4 +2,5 @@ export { default as campResource, detailedCampResource } from "./camp.resource";
 export { default as userResource, userCampResource } from "./user.resource";
 export { default as registrationResource } from "./registration.resource";
 export { default as templateResource } from "./template.resource";
+export { default as campManagerResource } from "./manager.resource";
 export { default as roomResource } from "./room.resource";
