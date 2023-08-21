746f027b9aa54f4b4195bd104d3393618435ac63
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-08-21 12:24:04 +0200

    Remove design import

--- src/boot/surveyJS.ts
+++ src/boot/surveyJS.ts
@@ -1,7 +1,6 @@
 import { boot } from 'quasar/wrappers';
 import { surveyPlugin } from 'survey-vue3-ui';
 import 'survey-core/survey.i18n';
-import 'survey-core/modern.min.css';
 
 export default boot(({ app }) => {
   app.use(surveyPlugin);
