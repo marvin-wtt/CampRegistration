08b992e8e255515127e89fbd22347377e14f0d1a
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-09-09 03:59:32 +0200

    Update test config

--- vitest.config.integration.ts
+++ vitest.config.integration.ts
@@ -1,23 +1,14 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ['tests/integration/**/*.test.ts'],
+    include: ["tests/integration/**/*.test.ts"],
     threads: false,
-    setupFiles: ['tests/utils/setup.ts']
+    setupFiles: ["tests/utils/setup.ts"],
   },
-  // resolve: {
-  //   alias: {
-  //     config: '/src/config',
-  //     controllers: '/src/controllers',
-  //     guards: '/src/guards',
-  //     jobs: '/src/jobs',
-  //     middleware: '/src/middleware',
-  //     resources: '/src/resources',
-  //     routes: '/src/routes',
-  //     services: '/src/services',
-  //     utils: '/src/utils',
-  //     validators: '/src/validators',
-  //   }
-  // }
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
