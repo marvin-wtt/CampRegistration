d9583bd8befb3ad12bd33c4308132054596f923a
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-09-22 02:02:20 +0200

    Refactor camp manager middleware

--- src/guards/camp.guard.ts
+++ src/guards/camp.guard.ts
@@ -4,15 +4,10 @@ import { routeModel } from "@/utils/verifyModel";
 import authUserId from "@/utils/authUserId";
 
 export const campManager = async (req: Request): Promise<boolean | string> => {
-  let userId = "";
-  try {
-    userId = authUserId(req);
-  } catch (error) {
-    return "Unauthenticated";
-  }
-
+  const userId = authUserId(req);
   const campId = routeModel(req.models.camp).id;
 
+  // TODO This guard is called very often. Is it possible to store the information in the token?
   return await managerService.campManagerExistsWithUserIdAndCampId(
     campId,
     userId
