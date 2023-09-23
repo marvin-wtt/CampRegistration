0910a810f518c98fef5e4f13e95808b00f089996
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-09-23 04:51:01 +0200

    Add file store translations

---
+++ src/i18n/en-US/stores/camp-files.ts
@@ -0,0 +1,16 @@
+export default {
+  fetch: {
+    error: 'Failed to fetch files',
+  },
+  create: {
+    progress: 'Uploading file...',
+    success: 'File uploaded successfully',
+    error: 'Failed to upload file',
+  },
+  delete: {
+    progress: 'Deleting file...',
+    success: 'File deleted successfully',
+    error: 'Failed to delete file',
+    invalid: 'Invalid file id',
+  },
+};
