13e192caa5f7d60f5ffcbf12426a6daee90df1da
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-09-25 03:33:26 +0200

    Wip: Add participant guard

---
+++ src/guards/participant.guard.ts
@@ -0,0 +1,11 @@
+import { Request } from "express";
+import { routeModel } from "@/utils/verifyModel";
+
+const participant = async (req: Request): Promise<boolean | string> => {
+  const registration = routeModel(req.models.registration);
+
+  // TODO Check, if the auth user is the participant
+  return false;
+};
+
+export default participant;
