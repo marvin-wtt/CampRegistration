cbc7511f096629d3bfa8a9aa381345f800e86d95
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-10-15 03:50:31 +0200

    Use safe delete dialog
    Use registration helper

--- src/components/campManagement/table/tableCells/ActionTableCell.vue
+++ src/components/campManagement/table/tableCells/ActionTableCell.vue
@@ -15,7 +15,7 @@
         @click="editItem"
       >
         <q-item-section>
-          {{ t('edit') }}
+          {{ t('option.edit') }}
         </q-item-section>
       </q-item>
       <q-item
@@ -25,7 +25,7 @@
         @click="accept"
       >
         <q-item-section>
-          {{ t('accept') }}
+          {{ t('option.accept') }}
         </q-item-section>
       </q-item>
       <q-separator />
@@ -36,7 +36,7 @@
         @click="deleteItem"
       >
         <q-item-section>
-          {{ t('delete') }}
+          {{ t('option.delete') }}
         </q-item-section>
       </q-item>
     </q-menu>
@@ -51,32 +51,49 @@ import EditResultComponent from 'components/campManagement/table/dialogs/EditRes
 import { useCampDetailsStore } from 'stores/camp-details-store';
 import { storeToRefs } from 'pinia';
 import { computed } from 'vue';
-import ConfirmDeleteDialog from 'components/campManagement/table/dialogs/ConfirmDeleteRegistrationDialog.vue';
 import { TableCellProps } from 'components/campManagement/table/tableCells/TableCellProps';
+import { Registration } from 'src/types/Registration';
+import { useRegistrationHelper } from 'src/composables/registrationHelper';
+import SafeDeleteDialog from 'components/common/dialogs/SafeDeleteDialog.vue';
+import { useRegistrationsStore } from 'stores/registration-store';
 
 const props = defineProps<TableCellProps>();
 const quasar = useQuasar();
 const { t } = useI18n();
 const campDetailStore = useCampDetailsStore();
+const registrationStore = useRegistrationsStore();
 const camp = storeToRefs(campDetailStore);
+const registrationHelper = useRegistrationHelper();
 
 const size = computed<string>(() => {
   return props.props.dense ? 'xs' : 'md';
 });
 
+const registration = computed<Registration>(() => {
+  return props.props.row as Registration;
+});
+
 const waitingList = computed<boolean>(() => {
-  const key = props.settings?.waitingListKey ?? 'waiting_list';
-  return props.props.row[key] == true;
+  return registrationHelper.waitingList(registration.value);
 });
 
 function deleteItem(): void {
-  quasar.dialog({
-      // TODO Use safe delete component instead
-    component: ConfirmDeleteDialog,
-    componentProps: {
-      result: props.props.row,
-    },
-  });
+  const value = registrationHelper.fullName(registration.value);
+
+  quasar
+    .dialog({
+      component: SafeDeleteDialog,
+      componentProps: {
+        title: t('dialog.delete.title'),
+        message: t('dialog.delete.message'),
+        label: t('dialog.delete.label'),
+        value: value,
+      },
+    })
+    .onOk(async () => {
+      const id = registration.value.id;
+      await registrationStore.deleteData(id);
+    });
 }
 
 function accept(): void {
@@ -89,28 +106,49 @@ function editItem(): void {
     component: EditResultComponent,
     componentProps: {
       questions: camp.data.value?.form,
-      result: props.props.row,
+      result: registration.value.data,
     },
   });
 }
 </script>
 
-<!-- TODO Add translations -->
-<i18n lang="yaml">
-en:
+<i18n lang="yaml" locale="em">
+option:
   edit: 'Edit'
   delete: 'Delete'
   accept: 'Accept registration'
 
-de:
+dialog:
+  delete:
+    title: 'Delete registration'
+    message: 'Once you delete the registration, all data about this person is lost.'
+    label: 'Name'
+</i18n>
+
+<i18n lang="yaml" locale="de">
+option:
   edit: 'Bearbeiten'
   delete: 'Löschen'
   accept: 'Anmeldung akzeptieren'
 
-fr:
+dialog:
+  delete:
+    title: 'Registrierung löschen'
+    message: 'Wenn Sie die Registrierung löschen, gehen alle Daten dieser Person verloren.'
+    label: 'Name'
+</i18n>
+
+<i18n lang="yaml" locale="fr">
+option:
   edit: 'Modifier'
   delete: 'Supprimer'
   accept: "Accepter l'inscription"
+
+dialog:
+  delete:
+    title: "Supprimer l'inscription"
+    message: "Une fois que vous supprimez l'inscription, toutes les données concernant cette personne sont perdues."
+    label: 'Nom'
 </i18n>
 
 <style scoped></style>
