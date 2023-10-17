c55796bee3fcab85017fb76434a5e079063c0a41
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-10-18 01:45:43 +0200

    Move files to frontend dir

---
+++ frontend/src/components/common/PageStateHandler.vue
@@ -0,0 +1,129 @@
+<template>
+  <q-page :padding="padding">
+    <div
+      v-if="loading || error"
+      class="absolute fit row justify-center"
+    >
+      <slot
+        v-if="loading"
+        name="loading"
+      >
+        <!-- TODO Can we use <q-inner-loading> instead ?-->
+        <div class="self-center">
+          <q-spinner
+            color="primary"
+            size="100px"
+          />
+        </div>
+      </slot>
+
+      <slot
+        v-else-if="error"
+        name="error"
+      >
+        <div class="text-center self-center">
+          <q-icon
+            name="error"
+            size="xl"
+          />
+
+          <p class="text-h4">
+            {{ props.error || 'Error' }}
+          </p>
+        </div>
+      </slot>
+    </div>
+
+    <slot
+      v-else
+      class="col self-baseline"
+    />
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+import { computed, onBeforeMount, onBeforeUnmount, onErrorCaptured, ref } from 'vue';
+import { onBeforeRouteLeave } from 'vue-router';
+import { useQuasar } from 'quasar';
+
+interface Props {
+  error?: string | string[] | null;
+  loading?: boolean;
+  padding?: boolean;
+  preventLeave?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  error: null,
+  loading: false,
+  padding: false,
+  preventLeave: false,
+});
+
+const quasar = useQuasar();
+
+const localError = ref(false);
+
+const error = computed<boolean>(() => {
+  return props.error != null || localError.value;
+});
+
+const loading = computed<boolean>(() => {
+  return props.loading;
+});
+
+const padding = computed<boolean>(() => {
+  return props.padding && !loading.value && !error.value;
+});
+
+onBeforeMount(() => {
+  window.addEventListener('beforeunload', preventPageLeave);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('beforeunload', preventPageLeave);
+});
+
+function preventPageLeave(event) {
+  if (!props.preventLeave) {
+    return;
+  }
+
+  event.preventDefault();
+  event.returnValue = '';
+}
+
+onBeforeRouteLeave((to, from, next) => {
+  if (!props.preventLeave) {
+    next();
+    return;
+  }
+
+  quasar
+    .dialog({
+      title: 'Unsaved changes',
+      message:
+        'You have unsaved changes. These changes will be lost if you proceed',
+      cancel: true,
+      persistent: true,
+    })
+    .onOk(() => {
+      next();
+    })
+    .onDismiss(() => {
+      next(false);
+    })
+    .onCancel(() => {
+      next(false);
+    });
+});
+
+onErrorCaptured((err, instance) => {
+  // TODO Handle gracefully
+  localError.value = true;
+});
+
+// TODO Not working...
+</script>
+
+<style scoped></style>
---
+++ frontend/src/services/TemplateService.ts
@@ -0,0 +1,51 @@
+import { TableTemplate, TemplateCreateData, TemplateUpdateData } from 'src/types/TableTemplate';
+import { api } from 'boot/axios';
+
+export function useTemplateService() {
+  async function fetchResultTemplates(
+    campId: string,
+  ): Promise<TableTemplate[]> {
+    const response = await api.get(`camps/${campId}/templates/`);
+
+    return response.data.data;
+  }
+
+  async function fetchResultTemplate(
+    campId: string,
+    templateId: string,
+  ): Promise<TableTemplate[]> {
+    const response = await api.get(`camps/${campId}/templates/${templateId}/`);
+
+    return response.data.data;
+  }
+
+  async function createResultTemplate(
+    campId: string,
+    data: TemplateCreateData,
+  ): Promise<void> {
+    await api.post(`camps/${campId}/templates/`, data);
+  }
+
+  async function updateResultTemplate(
+    campId: string,
+    templateId: string,
+    data: TemplateUpdateData,
+  ): Promise<void> {
+    await api.put(`camps/${campId}/templates/${templateId}/`, data);
+  }
+
+  async function deleteResultTemplate(
+    campId: string,
+    templateId: string,
+  ): Promise<void> {
+    await api.delete(`camps/${campId}/templates/${templateId}/`);
+  }
+
+  return {
+    fetchResultTemplates,
+    fetchResultTemplate,
+    createResultTemplate,
+    updateResultTemplate,
+    deleteResultTemplate,
+  };
+}
