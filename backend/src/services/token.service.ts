8be09ebea3f71cfe8705792d7af55fa2f875f15d
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-11-26 21:40:42 +0100

    Revoke other tokens when new one is requested

--- backend/src/services/token.service.ts
+++ backend/src/services/token.service.ts
@@ -41,6 +41,15 @@ const saveToken = async (
   });
 };
 
+const revokeTokens = async (userId: string, type?: TokenType) => {
+  return prisma.token.deleteMany({
+    where: {
+      userId,
+      type,
+    },
+  });
+};
+
 /**
  * Verify token and return token doc (or throw an error if it is not valid)
  * @param {string} token
@@ -138,17 +147,11 @@ const generateResetPasswordToken = async (
     config.jwt.resetPasswordExpirationMinutes,
     "minutes",
   );
-  const resetPasswordToken = generateToken(
-    user.id,
-    expires,
-    TokenType.RESET_PASSWORD,
-  );
-  await saveToken(
-    resetPasswordToken,
-    user.id,
-    expires,
-    TokenType.RESET_PASSWORD,
-  );
+  const type = TokenType.RESET_PASSWORD;
+  const resetPasswordToken = generateToken(user.id, expires, type);
+  await revokeTokens(user.id, type);
+  await saveToken(resetPasswordToken, user.id, expires, type);
+
   return resetPasswordToken;
 };
 
@@ -164,12 +167,11 @@ const generateVerifyEmailToken = async (
     config.jwt.verifyEmailExpirationMinutes,
     "minutes",
   );
-  const verifyEmailToken = generateToken(
-    user.id,
-    expires,
-    TokenType.VERIFY_EMAIL,
-  );
-  await saveToken(verifyEmailToken, user.id, expires, TokenType.VERIFY_EMAIL);
+  const type = TokenType.VERIFY_EMAIL;
+  const verifyEmailToken = generateToken(user.id, expires, type);
+  await revokeTokens(user.id, type);
+  await saveToken(verifyEmailToken, user.id, expires, type);
+
   return verifyEmailToken;
 };
 
