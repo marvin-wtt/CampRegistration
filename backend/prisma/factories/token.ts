dbc5f91d4471aba0034f3e175e4fcee9ba764187
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-11-27 02:25:41 +0100

    Add token factory

---
+++ backend/prisma/factories/token.ts
@@ -0,0 +1,24 @@
+import { fakerDE as faker } from "@faker-js/faker";
+import { Prisma, TokenType } from "@prisma/client";
+import prisma from "../../tests/utils/prisma";
+
+export const TokenFactory = {
+  build: (data: Partial<Prisma.TokenCreateInput>): Prisma.TokenCreateInput => {
+    return {
+      expiresAt: faker.date.future({
+        years: 1,
+      }),
+      blacklisted: false,
+      token: "",
+      type: faker.helpers.enumValue(TokenType),
+      ...data,
+      user: {},
+    };
+  },
+
+  create: async (data: Partial<Prisma.TokenCreateInput> = {}) => {
+    return prisma.token.create({
+      data: TokenFactory.build(data),
+    });
+  },
+};
