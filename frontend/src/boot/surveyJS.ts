b5919017a3b3a5523e326757702a4adb8ec3fb26
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-11-29 04:51:00 +0100

    Wip: Manually install common components

--- frontend/src/boot/surveyJS.ts
+++ frontend/src/boot/surveyJS.ts
@@ -2,7 +2,36 @@ import { boot } from 'quasar/wrappers';
 import { surveyPlugin } from 'survey-vue3-ui';
 import { surveyCreatorPlugin } from 'survey-creator-vue';
 import 'survey-core/survey.i18n';
-import '@camp-registration/common/form';
+import {
+  address,
+  country,
+  dateOfBirth,
+  role,
+} from '@camp-registration/common/form/questions';
+import {
+  htmlDate,
+  isAdult,
+  isMinor,
+  subtractYears,
+} from '@camp-registration/common/form/functions';
+import { campDataType } from '@camp-registration/common/form/property';
+import { ComponentCollection, FunctionFactory, Serializer } from 'survey-core';
+
+// TODO Why do I need this?
+ComponentCollection.Instance.add(address);
+ComponentCollection.Instance.add(country);
+ComponentCollection.Instance.add(dateOfBirth);
+ComponentCollection.Instance.add(role);
+
+FunctionFactory.Instance.register('isMinor', isMinor, false);
+FunctionFactory.Instance.register('isAdult', isAdult, false);
+FunctionFactory.Instance.register('subtractYears', subtractYears, false);
+FunctionFactory.Instance.register('htmlDate', htmlDate, false);
+
+Serializer.addProperty('question', campDataType);
+
+Serializer.getProperty('file', 'storeDataAsText').visible = false;
+Serializer.getProperty('file', 'storeDataAsText').defaultValue = false;
 
 export default boot(({ app }) => {
   app.use(surveyPlugin);
