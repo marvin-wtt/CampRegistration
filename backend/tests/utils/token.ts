d41bf1528abd0a1d7e7d2e8011e4b846732f7860
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-11-30 12:35:17 +0100

    Fix: Add missing token helpers

--- backend/tests/utils/token.ts
+++ backend/tests/utils/token.ts
@@ -2,13 +2,26 @@ import { TokenType, User } from "@prisma/client";
 import moment from "moment/moment";
 import jwt from "jsonwebtoken";
 
-export function generateAccessToken(user: User) {
+export const verifyToken = (token: string) => {
+  return jwt.verify(token, process.env.JWT_SECRET as string);
+};
+
+export const generateToken = (user: User, type: TokenType, data?: object) => {
   const payload = {
     sub: user.id,
     iat: moment().unix(),
     exp: moment().add("1", "minutes").unix(),
-    type: TokenType.ACCESS,
+    type,
+    ...data,
   };
 
   return jwt.sign(payload, process.env.JWT_SECRET as string);
-}
+};
+
+export const generateAccessToken = (user: User) => {
+  return generateToken(user, TokenType.ACCESS);
+};
+
+export const generateResetPasswordToken = (user: User) => {
+  return generateToken(user, TokenType.RESET_PASSWORD);
+};
