51e84d192076fe3843263faf5fca180a802e0494
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-11-30 13:14:49 +0100

    Add factories

---
+++ backend/prisma/factories/invitation.ts
@@ -0,0 +1,22 @@
+import { Prisma } from "@prisma/client";
+import prisma from "../../tests/utils/prisma";
+import { ulid } from "ulidx";
+import { faker } from "@faker-js/faker/locale/de";
+
+export const InvitationFactory = {
+  build: (
+    data: Partial<Prisma.InvitationCreateInput>,
+  ): Prisma.InvitationCreateInput => {
+    return {
+      id: ulid(),
+      email: faker.internet.email(),
+      ...data,
+    };
+  },
+
+  create: async (data: Partial<Prisma.InvitationCreateInput> = {}) => {
+    return prisma.invitation.create({
+      data: InvitationFactory.build(data),
+    });
+  },
+};
---
+++ backend/prisma/factories/manager.ts
@@ -0,0 +1,21 @@
+import { Prisma } from "@prisma/client";
+import prisma from "../../tests/utils/prisma";
+import { ulid } from "ulidx";
+
+export const CampManagerFactory = {
+  build: (
+    data: Partial<Prisma.CampManagerCreateInput>,
+  ): Prisma.CampManagerCreateInput => {
+    return {
+      id: ulid(),
+      camp: {},
+      ...data,
+    };
+  },
+
+  create: async (data: Partial<Prisma.CampManagerCreateInput> = {}) => {
+    return prisma.campManager.create({
+      data: CampManagerFactory.build(data),
+    });
+  },
+};
