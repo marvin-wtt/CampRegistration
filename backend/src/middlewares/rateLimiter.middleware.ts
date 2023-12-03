79bb6b6d37c05f5e2e2f77e9f41ba4f180eeeb72
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-03 03:06:27 +0100

    Expose rate limiter store

--- backend/src/middlewares/rateLimiter.middleware.ts
+++ backend/src/middlewares/rateLimiter.middleware.ts
@@ -1,12 +1,17 @@
-import rateLimit from "express-rate-limit";
+import { rateLimit, MemoryStore } from "express-rate-limit";
+
+// Expose store to reset it for tests
+export const store = new MemoryStore();
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 20,
   skipSuccessfulRequests: true,
+  store,
 });
 
 export const generalLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
   limit: 100, // limit each IP to 100 requests per windowMs
+  store,
 });
