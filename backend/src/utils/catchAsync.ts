bf194d10e98fae9f072a22402ed5d7e8bbb8abb8
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-03 04:37:35 +0100

    Remove unused method

--- backend/src/utils/catchAsync.ts
+++ backend/src/utils/catchAsync.ts
@@ -1,7 +1,6 @@
 import { RequestHandler } from "express";
 import { Request, Response, NextFunction } from "express";
 import { RequestParamHandler } from "express-serve-static-core";
-import logger from "@/config/logger";
 
 // TODO Can be removed with express v5.x
 export const catchRequestAsync =
@@ -20,15 +19,3 @@ export const catchParamAsync =
   ) => {
     Promise.resolve(fn(req, res, next, value, name)).catch((err) => next(err));
   };
-
-export const catchSilent = async <T>(
-  fn: () => Promise<T>,
-): Promise<T | null> => {
-  try {
-    return await fn();
-  } catch (error: unknown) {
-    logger.warn(error);
-  }
-
-  return null;
-};
