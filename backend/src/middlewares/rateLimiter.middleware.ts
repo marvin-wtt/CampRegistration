d5dc1d769d45e9f4373aceb184040588f445c709
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-03 03:40:26 +0100

    Update rate limiter middleware

--- backend/src/middlewares/rateLimiter.middleware.ts
+++ backend/src/middlewares/rateLimiter.middleware.ts
@@ -4,14 +4,16 @@ import { rateLimit, MemoryStore } from "express-rate-limit";
 export const store = new MemoryStore();
 
 export const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 20,
+  windowMs: 60 * 1000,
+  limit: 5,
   skipSuccessfulRequests: true,
+  keyGenerator: (request) => `Auth:${request.ip}`,
   store,
 });
 
 export const generalLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
   limit: 100, // limit each IP to 100 requests per windowMs
+  keyGenerator: (request) => `General:${request.ip}`,
   store,
 });
