8b8a5454b06be427a28286f361cd32c22c11a384
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-03 03:09:13 +0100

    Fix: Don't expose rate limiter store in index

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,6 +1,6 @@
 export { default as validate } from "./validate.middleware";
 export * from "./error.middleware";
-export * from "./rateLimiter.middleware";
+export { authLimiter, generalLimiter } from "./rateLimiter.middleware";
 export { default as guard } from "./guard.middleware";
 export * from "./auth.middleware";
 export { default as multipart } from "./multipart.middleware";
