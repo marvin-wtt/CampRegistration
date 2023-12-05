ed6e62e32daec67ec10ff9ccd2d7c2c53fc7e840
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-05 14:25:51 +0100

    Wip: Add deploy script

---
+++ post-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# This script should be executed on the deployment server
+
+# common
+cd ./common/ || exit
+npm install
+npm build
+
+# frontend
+cd ../frontend/ || exit
+npm install
+npm build
+
+# backend
+cd ../backend/ || exit
+npm install
+npx prisma migrate deploy
+npm build
+cp -R ../frontend/dist/ ./public
+
+# Restart Node.JS on Plesk
+# TODO This file might be in ../tmp/restart.txt
+touch tmp/restart.txt
