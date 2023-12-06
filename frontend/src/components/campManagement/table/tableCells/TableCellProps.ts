513f2dda6cbe4920235a7c6760dbd89d6771c01a
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-06 23:58:40 +0100

    Remove registration settings

--- frontend/src/components/campManagement/table/tableCells/TableCellProps.ts
+++ frontend/src/components/campManagement/table/tableCells/TableCellProps.ts
@@ -1,9 +1,7 @@
 import { QTableBodyCellProps } from 'src/types/quasar/QTableBodyCellProps';
-import { RegistrationSettings } from 'src/types/RegistrationSettings';
 
 export interface TableCellProps {
   props: QTableBodyCellProps;
-  settings?: RegistrationSettings;
   options?: object;
   printing: boolean;
 }
