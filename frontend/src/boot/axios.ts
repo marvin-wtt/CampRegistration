96cbdb44863d24ee054286c6a43ee48c856d6ecf
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-09 06:53:26 +0100

    Fix api url hardcoded

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -7,11 +7,14 @@ declare module '@vue/runtime-core' {
   }
 }
 
-const apiUrl = 'http://localhost:3000';
+// Resolve url depending on environment
+const apiUrl = window.location.hostname === 'localhost'
+  ? 'http://localhost:3000'
+  : window.origin;
 
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
+// If any client changes this (g  lobal) instance, it might be a
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
