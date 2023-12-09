858eeeedd0380c2fcd1734d39bdc94aff481a1bf
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-09 08:02:19 +0100

    Fix: Use ci for installation

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # This script should be executed on the deployment server
 export PATH=/opt/plesk/node/20/bin
 
-mpm install
+mpm ci
 npm run build --workspaces
 npm run db:migrate --workspace @camp-registration/api
 
