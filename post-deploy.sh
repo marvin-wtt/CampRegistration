22ce8b4c346fd21ca1411e38e4edee0d687b0d65
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-09 03:49:44 +0100

    Update deployment script

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
 # This script should be executed on the deployment server
-export PATH=/opt/plesk/node/21/bin
+export PATH=/opt/plesk/node/20/bin
 
-# common
-#cd ./common/ || exit
-#npm install
-#npm build
-
-# frontend
-cd ../frontend/ || exit
-npm install
-npm build
-
-# backend
-cd ../backend/ || exit
-npm install
-npx prisma migrate deploy
-npm build
-cp -R ../frontend/dist/ ./public
+mpm install
+npm run build --workspaces
+npm run db:migrate --workspace @camp-registration/api
 
 # Restart Node.JS on Plesk
-# TODO This file might be in ../tmp/restart.txt
 touch tmp/restart.txt
