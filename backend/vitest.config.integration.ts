e53acdd7b9ba5e91007b4a9f6587116a021ec4ff
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-10 20:13:52 +0100

    Fix test config

--- backend/vitest.config.integration.ts
+++ backend/vitest.config.integration.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     include: ["tests/integration/**/*.test.ts"],
-    threads: false,
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      },
+    },
     setupFiles: ["tests/utils/setup.ts"],
     coverage: {
       enabled: true,
@@ -11,11 +17,6 @@ export default defineConfig({
       provider: "v8",
       reporter: ["html", "text", "json", "clover"],
       reportsDirectory: "./tests/integration/coverage",
-    },
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
     },
   },
 });
