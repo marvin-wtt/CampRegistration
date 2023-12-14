535878026d352f8a4a3c583f856de2e43db7f869
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-14 10:58:40 +0100

    Stop mail server between tests

--- backend/tests/utils/mail-server.ts
+++ backend/tests/utils/mail-server.ts
@@ -1,6 +1,20 @@
 import smtpTester from "smtp-tester";
 
-const mailServer = smtpTester.init(587);
+interface CustomNodeJsGlobal extends Global {
+  mailServer: smtpTester.MailServer;
+}
+
+declare const global: CustomNodeJsGlobal;
+
+if (global.mailServer) {
+  global.mailServer.stop(() => {
+    global.mailServer = smtpTester.init(587);
+  });
+} else {
+  global.mailServer = smtpTester.init(587);
+}
+
+const mailServer = global.mailServer;
 
 export const receiveEmail = async (email: string) => {
   return mailServer.captureOne(email, {
