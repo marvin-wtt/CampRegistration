29b7e6dc2328112cb9c5782c2947542d27a987eb
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-20 03:38:53 +0100

    Add surveyjs localization

--- frontend/src/boot/surveyJS.ts
+++ frontend/src/boot/surveyJS.ts
@@ -2,6 +2,8 @@ import { boot } from 'quasar/wrappers';
 import { surveyPlugin } from 'survey-vue3-ui';
 import { surveyCreatorPlugin } from 'survey-creator-vue';
 import '@camp-registration/common/form';
+import 'survey-core/i18n/german';
+import 'survey-core/i18n/french';
 
 export default boot(({ app }) => {
   app.use(surveyPlugin);
