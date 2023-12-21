760f68c73463269eba267cdcf57f938e9771c882
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-21 04:23:39 +0100

    Add manage invitation translations

--- backend/src/i18n/de-DE/manager/index.ts
+++ backend/src/i18n/de-DE/manager/index.ts
@@ -1,5 +1,22 @@
 export default {
   email: {
-    // TODO Add translations
+    invitation: {
+      subject: "Camp-Einladung",
+      preview: "Du wurdest eingeladen, {{ camp.name }} zu verwalten",
+      text: {
+        title: "Hallo,",
+        information:
+          "Du wurdest eingeladen, {{ camp.name }} zu verwalten." +
+          "Wenn du bereits ein Konto hast, wird das Camp automatisch verfügbar sein." +
+          "Andernfalls erstelle bitte zuerst ein kostenloses Konto.",
+        button: "Camp verwalten",
+        greeting: "Viele Grüße,",
+        teamName: "$t(appName) Team",
+      },
+      footer: {
+        cause:
+          "$t(email:footer.cause) du von einem anderen Benutzer eingeladen wurdest.",
+      },
+    },
   },
 };
