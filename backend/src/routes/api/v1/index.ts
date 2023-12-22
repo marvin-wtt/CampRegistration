5644094e1ba46619bd5b53d881f2b2190ea22e6f
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-12-22 13:59:41 +0100

    Fix: Remove unused imports

--- backend/src/routes/api/v1/index.ts
+++ backend/src/routes/api/v1/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import config from 'config';
 import campRoutes from './camps/camp.routes';
 import authRoutes from './auth.routes';
 import profileRoutes from './profile.routes';
