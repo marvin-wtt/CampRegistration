9fa30efeba7cf866bc4c7f7e47a874a6023cb028
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-01-05 13:17:59 +0100

    Add surveyjs license

--- frontend/src/boot/surveyJS.ts
+++ frontend/src/boot/surveyJS.ts
@@ -4,8 +4,19 @@ import { surveyCreatorPlugin } from 'survey-creator-vue';
 import '@camp-registration/common/form';
 import 'survey-core/i18n/german';
 import 'survey-core/i18n/french';
+import { setLicenseKey } from 'survey-core';
+import * as process from 'process';
 
 export default boot(({ app }) => {
+  setLicense();
+
   app.use(surveyPlugin);
   app.use(surveyCreatorPlugin);
 });
+
+function setLicense() {
+  const licenseKey = process.env.SURVEYJS_LICENCE_KEY;
+  if (licenseKey) {
+    setLicenseKey(licenseKey);
+  }
+}
