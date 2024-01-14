aa7605bffaac3dd500443db0c2d2485686d6f26e
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-01-14 03:56:19 +0100

    Fix pluralism for log messages

--- backend/src/jobs/files.job.ts
+++ backend/src/jobs/files.job.ts
@@ -3,10 +3,10 @@ import logger from 'config/logger';
 
 export const deleteUnusedFiles = async () => {
   const fileCount = await fileService.deleteUnreferencedFiles();
-  logger.info(`Deleted ${fileCount} unused files from disk`);
+  logger.info(`Deleted ${fileCount} unused file(s) from disk`);
 };
 
 export const deleteTemporaryFiles = async () => {
   const fileCount = await fileService.deleteTempFiles();
-  logger.info(`Deleted ${fileCount} unused temporary files from disk`);
+  logger.info(`Deleted ${fileCount} unused temporary file(s) from disk`);
 };
