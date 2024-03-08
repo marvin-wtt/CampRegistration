915b22b0990b78b08504ef345ac4e9a79ee1d5d8
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-03-08 03:36:31 +0100

    Fix: Remove unrelated SQL statements

--- backend/prisma/migrations/20240308020406_drop_rooms_capacity_column/migration.sql
+++ backend/prisma/migrations/20240308020406_drop_rooms_capacity_column/migration.sql
@@ -5,11 +5,5 @@
   - You are about to drop the `program_events` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropForeignKey
-ALTER TABLE `program_events` DROP FOREIGN KEY `program_events_camp_id_fkey`;
-
 -- AlterTable
 ALTER TABLE `rooms` DROP COLUMN `capacity`;
-
--- DropTable
-DROP TABLE `program_events`;
