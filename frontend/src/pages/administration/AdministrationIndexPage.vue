0aac3a60142fd4bb77fbab16f5dd4283680e2535
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-07-24 22:11:24 +0200

    feat: Add administration card

--- frontend/src/pages/administration/AdministrationIndexPage.vue
+++ frontend/src/pages/administration/AdministrationIndexPage.vue
@@ -1,21 +1,25 @@
 <template>
   <q-page padding>
-    <div class="q-pa-md row items-start q-gutter-md">
-      <q-card>
-        <q-card-section>
-          {{ t('camps') }}
-        </q-card-section>
-      </q-card>
+    <div class="row q-col-gutter-md">
+      <administration-card
+        :label="t('users')"
+        :to="{ name: 'administration.users' }"
+      />
     </div>
-
-    <!-- TODO Add content -->
   </q-page>
 </template>
 
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n';
+import AdministrationCard from 'components/administration/AdministrationCard.vue';
 
 const { t } = useI18n();
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
+
+<i18n lang="yaml" locale="en">
+camps: 'Camps'
+users: 'Users'
+settings: 'Settings'
+</i18n>
