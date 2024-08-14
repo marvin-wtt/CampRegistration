55e0f32cad6c2ce9e35ce3999411090020716455
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-08-14 03:14:42 +0200

    fix: Remove unused comment

--- backend/prisma/migrations/20240308020406_drop_rooms_capacity_column/migration.sql
+++ backend/prisma/migrations/20240308020406_drop_rooms_capacity_column/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - You are about to drop the column `capacity` on the `rooms` table. All the data in the column will be lost.
-  - You are about to drop the `program_events` table. If the table is not empty, all the data it contains will be lost.
 
 */
 -- AlterTable
