33bd505331da6fcb706dd5c8efb53d334b03d62e
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-08-18 19:20:37 +0200

    fix: Hide number input arrows

--- frontend/src/layouts/CampManagementLayout.vue
+++ frontend/src/layouts/CampManagementLayout.vue
@@ -318,4 +318,17 @@ notifications: 'Notifications'
 
 ::-webkit-scrollbar-corner {
 }
+
+/* Hide number input arrows */
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type='number'] {
+  -moz-appearance: textfield;
+}
 </style>
