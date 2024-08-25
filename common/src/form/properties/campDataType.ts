06933651f218d726388aac654c58adf98138840f
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-08-25 21:22:27 +0200

    feat: Update property typings

--- common/src/form/properties/campDataType.ts
+++ common/src/form/properties/campDataType.ts
@@ -1,8 +1,16 @@
-export default {
+import { IJsonPropertyInfo } from 'survey-core';
+
+const propertyInfo: IJsonPropertyInfo = {
   name: 'campDataType',
+  className: 'question',
   type: 'campDataMapping',
   default: undefined,
   isRequired: false,
   category: 'general',
   visibleIndex: 3,
+};
+
+export default {
+  className: propertyInfo.className,
+  propertyInfo: propertyInfo,
 };
