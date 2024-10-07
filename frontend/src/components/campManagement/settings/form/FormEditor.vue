434470f76759659dceaa2699c6dc6497fccd87fc
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-10-08 01:25:30 +0200

    feat: Add restricted editor access

---
+++ frontend/src/components/campManagement/settings/form/FormEditor.vue
@@ -0,0 +1,301 @@
+<template>
+  <survey-creator-component :model="creator" />
+</template>
+
+<script lang="ts" setup>
+// Style
+import 'survey-core/defaultV2.min.css';
+import 'survey-creator-core/survey-creator-core.min.css';
+// JS
+import 'survey-core/survey.i18n';
+import 'survey-creator-core/survey-creator-core.i18n';
+
+import { watchEffect } from 'vue';
+import {
+  ICreatorOptions,
+  localization,
+  PropertyGridEditorCollection,
+  SurveyCreatorModel,
+} from 'survey-creator-core';
+import { useI18n } from 'vue-i18n';
+import campDataMapping from 'src/lib/surveyJs/properties/campDataMapping';
+import {
+  Base,
+  ITheme,
+  PageModel,
+  SurveyElement,
+  SurveyModel,
+} from 'survey-core';
+import showdown from 'showdown';
+import FileSelectionDialog from 'components/campManagement/settings/files/FileSelectionDialog.vue';
+import { Panel } from 'survey-core/typings/knockout/kopage';
+import type {
+  CampDetails,
+  ServiceFile,
+} from '@camp-registration/common/entities';
+import { useQuasar } from 'quasar';
+import { SurveyJSCampData } from '@camp-registration/common/src/entities';
+import { setVariables } from '@camp-registration/common/form';
+
+const props = defineProps<{
+  camp: CampDetails;
+  files: ServiceFile[];
+  restrictedAccess: boolean;
+  saveFormFunc: (form: SurveyJSCampData) => Promise<void>;
+  saveThemeFunc: (theme: ITheme) => Promise<void>;
+  saveFileFunc: (file: File) => Promise<string>;
+}>();
+
+const quasar = useQuasar();
+const { locale } = useI18n();
+
+// Custom properties
+PropertyGridEditorCollection.register(campDataMapping);
+
+// Add localization
+const deLocale = localization.getLocale('de');
+deLocale.qt.address = 'Adresse';
+deLocale.qt.country = 'Land';
+deLocale.qt.date_of_birth = 'Geburtstag';
+deLocale.qt.role = 'Rolle';
+deLocale.p.campDataType = 'Daten-Tag';
+deLocale.pehelp.campDataType =
+  'Wählen Sie aus, welche Art von Daten der Benutzer eingibt. ' +
+  'Die Informationen werden dem Dienst unabhängig vom ' +
+  'Feldnamen zur Verfügung gestellt.';
+
+const enLocale = localization.getLocale('en');
+enLocale.qt.address = 'Address';
+enLocale.qt.country = 'Country';
+enLocale.qt.date_of_birth = 'Birthday';
+enLocale.qt.rolle = 'Role';
+enLocale.p.campDataType = 'Data Tag';
+enLocale.pehelp.campDataType =
+  'Select what type of data the user enters. ' +
+  'The information makes information available to the service regardless of the ' +
+  'field name.';
+
+const frLocale = localization.getLocale('fr');
+frLocale.qt.address = 'Adresse';
+frLocale.qt.country = 'Pays';
+frLocale.qt.date_of_birth = 'Date de Naissance';
+frLocale.qt.role = 'Rôle';
+frLocale.p.campDataType = 'Étiquette de données';
+frLocale.pehelp.campDataType =
+  'Sélectionnez le type de données que l’utilisateur saisit. ' +
+  'Les informations sont mises à la disposition du service indépendamment du ' +
+  'nom du champ.';
+
+const creatorOptions: ICreatorOptions = {
+  showLogicTab: true,
+  showTranslationTab: true,
+  showEmbeddedSurveyTab: false,
+  isAutoSave: true,
+  themeForPreview: 'defaultV2',
+  showThemeTab: true,
+  showJSONEditorTab: !props.restrictedAccess,
+};
+
+const markdownConverter = new showdown.Converter({
+  openLinksInNewWindow: true,
+});
+
+const creator = new SurveyCreatorModel(creatorOptions);
+
+creator.JSON = props.camp.form;
+creator.theme = props.camp.themes['light'];
+
+if (props.restrictedAccess) {
+  const panelItem = creator.toolbox.getItemByName('panel');
+  // Allow restricted users to add only panels. If you want to hide the entire Toolbox, set `creator.showToolbox = false;`
+  creator.toolbox.clearItems();
+  creator.toolbox.addItem(panelItem);
+
+  // Change the default question type to "panel"
+  creator.currentAddQuestionType = 'panel';
+
+  creator.showAddQuestionButton = false;
+}
+
+watchEffect(() => {
+  creator.locale = locale.value.split(/[-_]/)[0];
+});
+
+creator.onPropertyValidationCustomError.add((_, options) => {
+  if (!['name', 'valueName'].includes(options.propertyName)) {
+    return;
+  }
+
+  // Internal variables start with _
+  if (options.value.startsWith('_')) {
+    options.error = 'Zero is not allowed here.';
+    return;
+  }
+
+  if (options.value === 0) {
+    options.error = 'Zero is not allowed here.';
+    return;
+  }
+
+  // Dots are used to access objects
+  if (options.value.includes('.')) {
+    options.error = 'Dots are not allowed here.';
+    return;
+  }
+});
+
+creator.saveSurveyFunc = (
+  saveNo: number,
+  callback: (saveNo: number, success: boolean) => void,
+) => {
+  props
+    .saveFormFunc(creator.JSON)
+    .then(() => {
+      callback(saveNo, true);
+    })
+    .catch(() => {
+      callback(saveNo, false);
+    });
+};
+
+creator.saveThemeFunc = (
+  saveNo: number,
+  callback: (saveNo: number, success: boolean) => void,
+) => {
+  const theme = creator.theme;
+
+  props
+    .saveThemeFunc(theme)
+    .then(() => {
+      callback(saveNo, true);
+    })
+    .catch(() => {
+      callback(saveNo, false);
+    });
+};
+
+creator.onSurveyInstanceCreated.add((_, options) => {
+  const survey: SurveyModel = options.survey;
+
+  if (options.area === 'preview-tab' || options.area === 'designer-tab') {
+    // Convert markdown to html
+    survey.onTextMarkdown.add((_, options) => {
+      const str = markdownConverter.makeHtml(options.text);
+      // Remove root paragraphs <p></p>
+      options.html = str.substring(3, str.length - 4);
+    });
+  }
+
+  if (options.area === 'preview-tab') {
+    // Set surveyID for dynamic link generation
+    survey.surveyId = props.camp.id;
+
+    setVariables(survey, props.camp);
+    survey.onLocaleChangedEvent.add((sender) => {
+      setVariables(sender, props.camp);
+    });
+  }
+});
+
+creator.onUploadFile.add((_, options) => {
+  const files = options.files;
+
+  const campId = props.camp.id;
+  if (!campId || files.length == 0) {
+    options.callback('error', '');
+    return;
+  }
+
+  const file = files[0];
+
+  props
+    .saveFileFunc(file)
+    .then((fileUrl) => options.callback('success', fileUrl))
+    .catch(() => options.callback('error', ''));
+});
+
+creator.onOpenFileChooser.add((_, options) => {
+  quasar
+    .dialog({
+      component: FileSelectionDialog,
+      componentProps: {
+        accept: 'image/*',
+      },
+    })
+    .onOk((files) => {
+      options.callback(files);
+    })
+    .onCancel(() => {
+      options.callback([]);
+    });
+});
+
+let previousColorPalette: string | undefined;
+creator.themeEditor.onThemeSelected.add((_, options) => {
+  const colorPalette = options.theme.colorPalette;
+  if (colorPalette === previousColorPalette) {
+    return;
+  }
+  previousColorPalette = colorPalette;
+
+  const themes = props.camp.themes;
+  if (themes && colorPalette && colorPalette in themes) {
+    const mewTheme = themes[colorPalette];
+    creator.themeEditor.model.setTheme(colorPalette, mewTheme);
+  }
+});
+
+creator.onElementAllowOperations.add((_, options) => {
+  if (!props.restrictedAccess) {
+    return;
+  }
+  // Disallow restricted users to change question types, delete questions, or copy them
+  options.allowChangeType = false;
+  options.allowCopy = false;
+  const obj = options.obj as SurveyElement;
+  if (obj.isQuestion) {
+    options.allowDelete = false;
+  }
+  if (obj.isPage || obj.isPanel) {
+    options.allowDelete = (obj as Panel | PageModel).questions.length === 0;
+  }
+});
+
+creator.onCollectionItemAllowOperations.add((_, options) => {
+  if (!props.restrictedAccess) {
+    return;
+  }
+  // Disallow restricted users to delete columns via adorers on the design surface
+  options.allowDelete = !isObjColumn(options.item);
+});
+
+creator.onConfigureTablePropertyEditor.add((_, options) => {
+  if (!props.restrictedAccess) {
+    return;
+  }
+  // Disallow restricted users to add or delete matrix columns via the Property Grid
+  options.allowAddRemoveItems = options.propertyName !== 'columns';
+});
+
+creator.onGetPropertyReadOnly.add((_, options) => {
+  if (!props.restrictedAccess) {
+    return;
+  }
+  // Disallow restricted users to change cell question type in matrices
+  if (options.property.name === 'cellType') {
+    options.readOnly = true;
+  }
+  // Disallow restricted users to modify the `name` property for questions and matrix columns
+  const obj = options.obj as SurveyElement;
+  const disallowedProperties = ['name', 'campDataType'];
+  if (disallowedProperties.includes(options.property.name)) {
+    options.readOnly = obj.isQuestion || isObjColumn(options.obj);
+  }
+});
+
+function isObjColumn(obj: Base) {
+  return !!obj && obj.getType() === 'matrixdropdowncolumn';
+}
+</script>
+
+<style scoped></style>
--- frontend/src/pages/campManagement/settings/FormEditPage.vue
+++ frontend/src/pages/campManagement/settings/FormEditPage.vue
@@ -3,172 +3,94 @@
     :error="error"
     :loading="loading"
   >
-    <survey-creator-component :model="creator" />
+    <form-editor
+      v-if="showEditor && campData && campFiles"
+      :camp="campData"
+      :files="campFiles"
+      :restricted-access="restrictedAccess"
+      :save-form-func="saveForm"
+      :save-theme-func="saveTheme"
+      :save-file-func="saveFile"
+    />
   </page-state-handler>
 </template>
 
 <script lang="ts" setup>
-// Style
-import 'survey-core/defaultV2.min.css';
-import 'survey-creator-core/survey-creator-core.min.css';
-// JS
-import 'survey-core/survey.i18n';
-import 'survey-creator-core/survey-creator-core.i18n';
-
 import PageStateHandler from 'components/common/PageStateHandler.vue';
-import { computed, onMounted, ref, watchEffect } from 'vue';
-import {
-  localization,
-  PropertyGridEditorCollection,
-  SurveyCreatorModel,
-} from 'survey-creator-core';
+import { computed, onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { startAutoDataUpdate } from 'src/composables/survey';
 import { useCampDetailsStore } from 'stores/camp-details-store';
-import campDataMapping from 'src/lib/surveyJs/properties/campDataMapping';
 import { useCampFilesStore } from 'stores/camp-files-store';
-import { SurveyModel } from 'survey-core';
 import { storeToRefs } from 'pinia';
-import showdown from 'showdown';
 import { useAPIService } from 'src/services/APIService';
 import { useQuasar } from 'quasar';
-import FileSelectionDialog from 'components/campManagement/settings/files/FileSelectionDialog.vue';
+import { useRegistrationsStore } from 'stores/registration-store.ts';
+import FormEditor from 'components/campManagement/settings/form/FormEditor.vue';
+import type { SurveyJSCampData } from '@camp-registration/common/src/entities';
+import { ITheme } from 'survey-core';
+import EditorRestrictedAccessDialog from 'components/campManagement/settings/form/EditorRestrictedAccessDialog.vue';
 
 const quasar = useQuasar();
 const api = useAPIService();
 const campDetailsStore = useCampDetailsStore();
 const campFileStore = useCampFilesStore();
+const registrationStore = useRegistrationsStore();
 const { data: campData } = storeToRefs(campDetailsStore);
 const { data: campFiles } = storeToRefs(campFileStore);
-const { locale } = useI18n();
+const { t } = useI18n();
 
-// Custom properties
-PropertyGridEditorCollection.register(campDataMapping);
-
-// Add localization
-const deLocale = localization.getLocale('de');
-deLocale.qt.address = 'Adresse';
-deLocale.qt.country = 'Land';
-deLocale.qt.date_of_birth = 'Geburtstag';
-deLocale.qt.role = 'Rolle';
-deLocale.p.campDataType = 'Daten-Tag';
-deLocale.pehelp.campDataType =
-  'Wählen Sie aus, welche Art von Daten der Benutzer eingibt. ' +
-  'Die Informationen werden dem Dienst unabhängig vom ' +
-  'Feldnamen zur Verfügung gestellt.';
-
-const enLocale = localization.getLocale('en');
-enLocale.qt.address = 'Address';
-enLocale.qt.country = 'Country';
-enLocale.qt.date_of_birth = 'Birthday';
-enLocale.qt.rolle = 'Role';
-enLocale.p.campDataType = 'Data Tag';
-enLocale.pehelp.campDataType =
-  'Select what type of data the user enters. ' +
-  'The information makes information available to the service regardless of the ' +
-  'field name.';
-
-const frLocale = localization.getLocale('fr');
-frLocale.qt.address = 'Adresse';
-frLocale.qt.country = 'Pays';
-frLocale.qt.date_of_birth = 'Date de Naissance';
-frLocale.qt.role = 'Rôle';
-frLocale.p.campDataType = 'Étiquette de données';
-frLocale.pehelp.campDataType =
-  'Sélectionnez le type de données que l’utilisateur saisit. ' +
-  'Les informations sont mises à la disposition du service indépendamment du ' +
-  'nom du champ.';
+const showEditor = ref<boolean>(false);
+let restrictedAccess = ref<boolean>(false);
 
 const loading = computed<boolean>(() => {
-  return campDetailsStore.isLoading || campFileStore.isLoading;
+  return (
+    campDetailsStore.isLoading ||
+    campFileStore.isLoading ||
+    registrationStore.isLoading
+  );
 });
 
 const error = computed(() => {
-  return campDetailsStore.error || campFileStore.error;
-});
-
-const creatorOptions = {
-  showLogicTab: true,
-  showTranslationTab: true,
-  showEmbeddedSurveyTab: false,
-  isAutoSave: true,
-  themeForPreview: 'defaultV2',
-  showThemeTab: true,
-};
-
-const markdownConverter = new showdown.Converter({
-  openLinksInNewWindow: true,
-});
-
-const creator = new SurveyCreatorModel(creatorOptions);
-const previewModel = ref<SurveyModel>();
-
-startAutoDataUpdate(previewModel, campData, campFiles);
-
-watchEffect(() => {
-  creator.locale = locale.value.split(/[-_]/)[0];
+  return (
+    campDetailsStore.error || campFileStore.error || registrationStore.error
+  );
 });
 
 onMounted(async () => {
   await campDetailsStore.fetchData();
   await campFileStore.fetchData();
+  await registrationStore.fetchData();
 
   if (!campDetailsStore.data) {
     return;
   }
 
-  creator.JSON = campDetailsStore.data.form;
-  creator.theme = campDetailsStore.data.themes['light'];
-});
-
-creator.onPropertyValidationCustomError.add((_, options) => {
-  if (!['name', 'valueName'].includes(options.propertyName)) {
-    return;
-  }
-
-  // TODO Translate errors
-
-  // Internal variables start with _
-  if (options.value.startsWith('_')) {
-    options.error = 'Zero is not allowed here.';
-    return;
-  }
-
-  if (options.value === 0) {
-    options.error = 'Zero is not allowed here.';
-    return;
-  }
-
-  // Dots are used to access objects
-  if (options.value.includes('.')) {
-    options.error = 'Dots are not allowed here.';
-    return;
+  if (registrationStore.data && registrationStore.data.length > 0) {
+    quasar
+      .dialog({ component: EditorRestrictedAccessDialog })
+      .onOk(() => {
+        restrictedAccess.value = true;
+      })
+      .onCancel(() => {
+        restrictedAccess.value = false;
+      })
+      .onDismiss(() => {
+        showEditor.value = true;
+      });
+  } else {
+    showEditor.value = true;
   }
 });
 
-creator.saveSurveyFunc = (
-  saveNo: number,
-  callback: (saveNo: number, success: boolean) => void,
-) => {
+async function saveForm(form: SurveyJSCampData): Promise<void> {
   const data = {
-    form: creator.JSON,
+    form,
   };
 
-  campDetailsStore
-    .updateData(data, 'none')
-    .then(() => {
-      callback(saveNo, true);
-    })
-    .catch(() => {
-      callback(saveNo, false);
-    });
-};
+  await campDetailsStore.updateData(data, 'none');
+}
 
-creator.saveThemeFunc = (
-  saveNo: number,
-  callback: (saveNo: number, success: boolean) => void,
-) => {
-  const theme = creator.theme;
+async function saveTheme(theme: ITheme): Promise<void> {
   const colorPlatte = theme.colorPalette ?? 'light';
 
   const data = {
@@ -178,109 +100,37 @@ creator.saveThemeFunc = (
     },
   };
 
-  campDetailsStore
-    .updateData(data, 'none')
-    .then(() => {
-      callback(saveNo, true);
-    })
-    .catch(() => {
-      callback(saveNo, false);
-    });
-};
+  await campDetailsStore.updateData(data, 'none');
+}
 
-creator.onSurveyInstanceCreated.add((_, options) => {
-  const survey: SurveyModel = options.survey;
-
-  if (options.area === 'preview-tab' || options.area === 'designer-tab') {
-    // Convert markdown to html
-    survey.onTextMarkdown.add((survey, options) => {
-      const str = markdownConverter.makeHtml(options.text);
-      // Remove root paragraphs <p></p>
-      options.html = str.substring(3, str.length - 4);
-    });
-  }
-
-  if (options.area === 'preview-tab') {
-    // Set surveyID for dynamic link generation
-    survey.surveyId = campData.value?.id ?? '';
-
-    previewModel.value = survey;
-  }
-});
-
-creator.onUploadFile.add((_, options) => {
-  const files = options.files;
-
+async function saveFile(file: File): Promise<string> {
   const campId = campData.value?.id;
-  if (!campId || files.length == 0) {
-    options.callback('error', '');
-    return;
-  }
 
-  // TODO Why is it an array and not a single file? The callback only accepts a single link as parameter
-  const file = files[0];
+  if (!campId) {
+    throw 'Camp not loaded';
+  }
 
   // When file is selected via custom picker, then the file is already present on the server
   if ('id' in file && typeof file.id === 'string') {
-    const url = campFileStore.getUrl(file.id, campId);
-    options.callback('success', url);
-    return;
+    return campFileStore.getUrl(file.id, campId);
   }
 
-  api
-    .createCampFile(campId, {
-      name: file.name.replace(/\.[^/.]+$/, ''),
-      field: crypto.randomUUID(),
-      file,
-      accessLevel: 'public',
-    })
-    .then((file) => {
-      const url = campFileStore.getUrl(file.id, campId);
-      options.callback('success', url);
-    })
-    .catch(() => {
-      options.callback('error', '');
-    });
-});
+  const newFile = await api.createCampFile(campId, {
+    name: file.name.replace(/\.[^/.]+$/, ''),
+    field: crypto.randomUUID(),
+    file,
+    accessLevel: 'public',
+  });
 
-creator.onOpenFileChooser.add((_, options) => {
-  quasar
-    .dialog({
-      component: FileSelectionDialog,
-      componentProps: {
-        accept: 'image/*',
-      },
-    })
-    .onOk((files) => {
-      options.callback(files);
-    })
-    .onCancel(() => {
-      options.callback([]);
-    });
-});
-
-// TODO Files need to be deleted as well
-
-let previousColorPalette: string | undefined;
-creator.themeEditor.onThemeSelected.add((sender, options) => {
-  const colorPalette = options.theme.colorPalette;
-  if (colorPalette === previousColorPalette) {
-    return;
-  }
-  previousColorPalette = colorPalette;
-
-  const themes = campDetailsStore.data?.themes;
-  if (themes && colorPalette && colorPalette in themes) {
-    const mewTheme = themes[colorPalette];
-    creator.themeEditor.model.setTheme(colorPalette, mewTheme);
-  }
-});
+  return campFileStore.getUrl(newFile.id, campId);
+}
 </script>
 
 <style>
 .svc-creator {
   position: absolute;
 }
+
 /* Creator popups should be over navigation bar */
 .sv-popup {
   z-index: 5000;
