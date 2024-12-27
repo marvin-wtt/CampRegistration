f5bf16fcc08244d917c57f6dd3a734686f7216ec
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-12-27 02:20:37 +0100

    fix: Update seeder

--- backend/prisma/seeders/registration.seeder.ts
+++ backend/prisma/seeders/registration.seeder.ts
@@ -1,62 +1,11 @@
-import { type Prisma, PrismaClient } from '@prisma/client';
-import data from './json/registrations.json';
-import { ulid } from '../../src/utils/ulid';
+import { PrismaClient } from '@prisma/client';
+
 const name = 'registration';
+
 const run = (prisma: PrismaClient) => {
-  const campId = '01H4BK6DFQAVVB5TDS5BJ1AB95 ';
-
-  const registrations: Prisma.RegistrationCreateManyInput[] = [];
-
-  for (const registration of data) {
-    // Boolean checkboxes
-    convertBooleanCheckbox('waiting_list', registration);
-    convertBooleanCheckbox('agreement_rules', registration);
-    convertBooleanCheckbox('agreement_privacy', registration);
-    convertBooleanCheckbox('agreement_forward_list_participants', registration);
-    convertBooleanCheckbox(
-      'agreement_general_terms_and_conditions',
-      registration,
-    );
-    // Integers
-    convertStringToInt('guardian_permission_leave', registration);
-
-    // @ts-ignore
-    registration.role = 'participant';
-
-    registrations.push({
-      id: ulid(),
-      campId: campId,
-      data: registration,
-    });
-  }
-
-  return prisma.registration.createMany({
-    data: registrations,
-  });
+  // TODO seed data
+  return Promise.resolve();
 };
-
-function convertBooleanCheckbox(
-  key: string,
-  registration: Record<string, any>,
-): void {
-  if (!(key in registration) || !Array.isArray(registration[key])) {
-    return;
-  }
-
-  registration[key] =
-    registration[key].length === 1 && registration[key][0] === '1';
-}
-
-function convertStringToInt(
-  key: string,
-  registration: Record<string, any>,
-): void {
-  if (!(key in registration)) {
-    return;
-  }
-
-  registration[key] = parseInt(registration[key]);
-}
 
 export default {
   name,
--- backend/prisma/seeders/template.seeder.ts
+++ backend/prisma/seeders/template.seeder.ts
@@ -1,24 +1,10 @@
-import { type Prisma, PrismaClient } from '@prisma/client';
-import data from './json/templates.json';
-import { ulid } from '../../src/utils/ulid';
+import { PrismaClient } from '@prisma/client';
+
 const name = 'template';
 
 const run = (prisma: PrismaClient) => {
-  const campId = '01H4BK6DFQAVVB5TDS5BJ1AB95 ';
-
-  const templates: Prisma.TemplateCreateManyInput[] = [];
-
-  for (const template of data) {
-    templates.push({
-      id: ulid(),
-      campId: campId,
-      data: template,
-    });
-  }
-
-  return prisma.template.createMany({
-    data: templates,
-  });
+  // TODO seed data
+  return Promise.resolve();
 };
 
 export default {
