54cfa0ca2555e787ced9112c99a69b17c13e836f
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-01-10 02:27:11 +0100

    fix: Remove logging

--- backend/tests/integration/camp.test.ts
+++ backend/tests/integration/camp.test.ts
@@ -363,7 +363,7 @@ describe('/api/v1/camps', () => {
         .post(`/api/v1/camps/`)
         .send(data)
         .auth(accessToken, { type: 'bearer' })
-        .expectOrPrint(201);
+        .expect(201);
 
       // Test response
       await assertCampCreated(data, body);
@@ -760,7 +760,7 @@ describe('/api/v1/camps', () => {
         .patch(`/api/v1/camps/${camp.id}`)
         .send(dataA)
         .auth(accessToken, { type: 'bearer' })
-        .expectOrPrint(200);
+        .expect(200);
 
       expect(bodyA).toHaveProperty('data.freePlaces', 8);
 
@@ -817,7 +817,7 @@ describe('/api/v1/camps', () => {
             .patch(`/api/v1/camps/${camp.id}`)
             .send(data)
             .auth(accessToken, { type: 'bearer' })
-            .expectOrPrint(expected);
+            .expect(expected);
         },
       );
     });
