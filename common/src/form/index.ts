ae06705ed4e2304aca28624d6b74fd4d3fa2b63b
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-01-15 08:28:57 +0100

    fix: Surveyjs esm import

--- common/src/form/index.ts
+++ common/src/form/index.ts
@@ -2,29 +2,25 @@ import questions from './questions/index.js';
 import functions from './functions/index.js';
 import properties from './properties/index.js';
 import { setVariables } from './variables/index.js';
+import pkg from 'survey-core';
+const { Serializer, ComponentCollection, FunctionFactory } = pkg;
 
-(async () => {
-  const { Serializer, ComponentCollection, FunctionFactory } = await import(
-    'survey-core'
-  );
+for (const component of questions) {
+  ComponentCollection.Instance.remove(component.name);
+  ComponentCollection.Instance.add(component);
+}
 
-  for (const component of questions) {
-    ComponentCollection.Instance.remove(component.name);
-    ComponentCollection.Instance.add(component);
-  }
+for (const fn of functions) {
+  FunctionFactory.Instance.unregister(fn.name);
+  FunctionFactory.Instance.register(fn.name, fn.func, fn.isAsync);
+}
 
-  for (const fn of functions) {
-    FunctionFactory.Instance.unregister(fn.name);
-    FunctionFactory.Instance.register(fn.name, fn.func, fn.isAsync);
-  }
+for (const property of properties) {
+  Serializer.removeProperty(property.className, property.propertyInfo.name);
+  Serializer.addProperty(property.className, property.propertyInfo);
+}
 
-  for (const property of properties) {
-    Serializer.removeProperty(property.className, property.propertyInfo.name);
-    Serializer.addProperty(property.className, property.propertyInfo);
-  }
-
-  Serializer.getProperty('file', 'storeDataAsText').visible = false;
-  Serializer.getProperty('file', 'storeDataAsText').defaultValue = false;
-})();
+Serializer.getProperty('file', 'storeDataAsText').visible = false;
+Serializer.getProperty('file', 'storeDataAsText').defaultValue = false;
 
 export { setVariables };
