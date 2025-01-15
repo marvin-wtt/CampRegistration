4150df21cab9da22f4733a8cbb945c5415f98219
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-01-15 04:06:04 +0100

    fix: Remove obsolete seeder

--- backend/prisma/seeders/user.seeder.ts
+++ backend/prisma/seeders/user.seeder.ts
@@ -1,13 +1,14 @@
 import { PrismaClient, PrismaPromise } from '@prisma/client';
 
 const name = 'user';
+
 const run = (prisma: PrismaClient): PrismaPromise<unknown> => {
   return prisma.user.create({
     data: {
       id: '01H4BK7J4WV75DZNAQBHMM99MA',
       name: 'John Doe',
       email: 'john@example.com',
-      password: '$2a$12$f4dwwAVJRqlz4okStqitmuC5UKrDhWMFjZEtviqrEJp4.in2uFPAK', // "password"
+      password: 'password',
       emailVerified: true,
     },
   });
