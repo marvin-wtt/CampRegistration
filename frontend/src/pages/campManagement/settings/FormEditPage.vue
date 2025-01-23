0a2b6267b52a4bd88b99e172c0e87449f3710e05
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-01-23 14:09:33 +0100

    fix: Remove unused vars

--- frontend/src/pages/campManagement/settings/FormEditPage.vue
+++ frontend/src/pages/campManagement/settings/FormEditPage.vue
@@ -21,7 +21,6 @@ import { computed, ref } from 'vue';
 import { useCampDetailsStore } from 'stores/camp-details-store';
 import { useCampFilesStore } from 'stores/camp-files-store';
 import { storeToRefs } from 'pinia';
-import { useAPIService } from 'src/services/APIService';
 import { useQuasar } from 'quasar';
 import { useRegistrationsStore } from 'stores/registration-store';
 import FormEditor from 'components/campManagement/settings/form/FormEditor.vue';
@@ -30,7 +29,6 @@ import type { ITheme } from 'survey-core';
 import EditorRestrictedAccessDialog from 'components/campManagement/settings/form/EditorRestrictedAccessDialog.vue';
 
 const quasar = useQuasar();
-const api = useAPIService();
 const campDetailsStore = useCampDetailsStore();
 const campFileStore = useCampFilesStore();
 const registrationStore = useRegistrationsStore();
