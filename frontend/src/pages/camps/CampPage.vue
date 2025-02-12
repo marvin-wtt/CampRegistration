8b4cb8280e531020c0b5f9043dc2dac5d371eaa6
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-02-12 23:31:18 +0100

    feat: Remove store usage for camp page

--- frontend/src/pages/camps/CampPage.vue
+++ frontend/src/pages/camps/CampPage.vue
@@ -1,13 +1,13 @@
 <template>
   <page-state-handler
-    :loading="loading"
-    :error="error"
+    :loading
+    :error
     class="row justify-center"
     :style="{ backgroundColor: bgColor }"
   >
     <registration-form
-      v-if="campData"
-      :camp-details="campData"
+      v-if="camp"
+      :camp-details="camp"
       :submit-fn="submit"
       :upload-file-fn="uploadFile"
       class="col-xs-12 col-sm-12 col-md-8 col-lg-6 col-xl-6"
@@ -18,43 +18,79 @@
 
 <script lang="ts" setup>
 import PageStateHandler from 'components/common/PageStateHandler.vue';
-import { useCampDetailsStore } from 'stores/camp-details-store';
-import { computed, ref } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import { useMeta } from 'quasar';
-import { useRegistrationsStore } from 'stores/registration-store';
 import { useObjectTranslation } from 'src/composables/objectTranslation';
-import { storeToRefs } from 'pinia';
 import RegistrationForm from 'components/common/RegistrationForm.vue';
+import type { CampDetails } from '@camp-registration/common/entities';
+import { isAPIServiceError, useAPIService } from 'src/services/APIService';
+import { useRoute } from 'vue-router';
+import { v7 as uuid } from 'uuid';
 
 const { to } = useObjectTranslation();
-const registrationStore = useRegistrationsStore();
-const campDetailsStore = useCampDetailsStore();
-const { data: campData } = storeToRefs(campDetailsStore);
+const api = useAPIService();
+const route = useRoute();
 
 const bgColor = ref<string>();
+const camp = ref<CampDetails | undefined>();
+const loading = ref<boolean>(false);
+const error = ref<string | null>(null);
+
+onBeforeMount(async () => {
+  await init();
+});
 
 useMeta(() => {
   return {
-    title: to(campDetailsStore.data?.name),
+    title: to(camp.value?.name),
   };
 });
 
-campDetailsStore.fetchData();
+async function init() {
+  try {
+    loading.value = true;
 
-const loading = computed<boolean>(() => {
-  return campDetailsStore.isLoading;
-});
+    const id = route.params.camp;
+    if (typeof id !== 'string') {
+      error.value = 'Invalid route params. Missing camp id.';
+      return;
+    }
 
-const error = computed(() => {
-  return campDetailsStore.error;
-});
+    camp.value = await api.fetchCamp(id);
+  } catch (err) {
+    camp.value = undefined;
+
+    const apiError = isAPIServiceError(err) ? err : null;
+    if (apiError == null) {
+      error.value = 'Unknown error';
+      return;
+    }
+
+    switch (apiError.response?.status) {
+      case 401:
+      case 403:
+        // TODO
+        break;
+      case 404:
+        // TODO
+        break;
+      default:
+        error.value = apiError.message;
+    }
+  } finally {
+    loading.value = false;
+  }
+}
 
 async function submit(campId: string, formData: Record<string, unknown>) {
-  await registrationStore.storeData(campId, { data: formData });
+  await api.createRegistration(campId, { data: formData });
 }
 
 async function uploadFile(file: File): Promise<string> {
-  const serviceFile = await registrationStore.storeFile(file);
+  const serviceFile = await api.createTemporaryFile({
+    file,
+    field: uuid(),
+  });
 
   if (serviceFile.field) {
     return `${serviceFile.id}#${serviceFile.field}`;
