371a75c4f8153152ba7f39ad825c61b63aeb1c4e
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-03-13 02:11:33 +0100

    feat: Convert to class based

--- backend/src/app/manager/manager.resource.ts
+++ backend/src/app/manager/manager.resource.ts
@@ -1,27 +1,26 @@
 import { CampManager, Invitation, User } from '@prisma/client';
-import type { CampManager as CampManagerResource } from '@camp-registration/common/entities';
+import type { CampManager as CampManagerData } from '@camp-registration/common/entities';
+import { JsonResource } from '#core/resource/JsonResource.js';
 
-interface ManagerWithRelationships extends CampManager {
+export interface ManagerWithRelationships extends CampManager {
   user: User | null;
   invitation: Invitation | null;
 }
 
-const managerResource = (
-  manager: ManagerWithRelationships,
-): CampManagerResource => {
-  const { user, invitation } = manager;
-
-  const email: string = invitation ? invitation.email : (user?.email ?? '');
-  const role = 'manager';
-
-  return {
-    id: manager.id,
-    name: user ? user.name : null,
-    email,
-    status: user ? 'accepted' : 'pending',
-    role,
-    expiresAt: manager.expiresAt?.toISOString() ?? null,
-  };
-};
-
-export default managerResource;
+export class ManagerResource extends JsonResource<
+  ManagerWithRelationships,
+  CampManagerData
+> {
+  transform(): CampManagerData {
+    return {
+      id: this.data.id,
+      name: this.data.user ? this.data.user.name : null,
+      email: this.data.invitation
+        ? this.data.invitation.email
+        : (this.data.user?.email ?? ''),
+      status: this.data.user ? 'accepted' : 'pending',
+      role: 'manager',
+      expiresAt: this.data.expiresAt?.toISOString() ?? null,
+    };
+  }
+}
--- backend/src/app/manager/manager.service.ts
+++ backend/src/app/manager/manager.service.ts
@@ -1,139 +1,119 @@
 import prisma from '#client.js';
 
-const campManagerExistsWithUserIdAndCampId = async (
-  campId: string,
-  userId: string,
-): Promise<boolean> => {
-  return prisma.campManager
-    .findFirst({
+class ManagerService {
+  async campManagerExistsWithUserIdAndCampId(campId: string, userId: string) {
+    return prisma.campManager
+      .findFirst({
+        where: {
+          campId,
+          userId,
+        },
+      })
+      .then((value) => value !== null);
+  }
+
+  async getManagers(campId: string) {
+    return prisma.campManager.findMany({
+      where: { campId },
+      include: {
+        invitation: true,
+        user: true,
+      },
+    });
+  }
+
+  async getManagerById(campId: string, id: string) {
+    return prisma.campManager.findFirst({
+      where: { id, campId },
+    });
+  }
+
+  async getManagerByUserId(campId: string, userId: string) {
+    return prisma.campManager.findFirst({
+      where: { userId, campId },
+    });
+  }
+
+  async getManagerByEmail(campId: string, email: string) {
+    return prisma.campManager.findFirst({
       where: {
         campId,
-        userId,
+        OR: [{ user: { email } }, { invitation: { email } }],
       },
-    })
-    .then((value) => value !== null);
-};
+    });
+  }
 
-const getManagers = async (campId: string) => {
-  return prisma.campManager.findMany({
-    where: { campId },
-    include: {
-      invitation: true,
-      user: true,
-    },
-  });
-};
-
-const getManagerById = async (campId: string, id: string) => {
-  return prisma.campManager.findFirst({
-    where: { id, campId },
-  });
-};
-
-const getManagerByUserId = async (campId: string, userId: string) => {
-  return prisma.campManager.findFirst({
-    where: { userId, campId },
-  });
-};
-
-const getManagerByEmail = async (campId: string, email: string) => {
-  return prisma.campManager.findFirst({
-    where: {
-      campId,
-      OR: [{ user: { email } }, { invitation: { email } }],
-    },
-  });
-};
-
-const resolveManagerInvitations = async (email: string, userId: string) => {
-  await prisma.campManager.updateMany({
-    where: {
-      invitation: {
-        email,
-      },
-    },
-    data: {
-      userId,
-    },
-  });
-
-  await prisma.invitation.deleteMany({
-    where: {
-      email,
-    },
-  });
-};
-
-const addManager = async (
-  campId: string,
-  userId: string,
-  expiresAt?: string,
-) => {
-  return prisma.campManager.create({
-    data: {
-      campId,
-      userId,
-      expiresAt,
-    },
-    include: {
-      user: true,
-      invitation: true,
-    },
-  });
-};
-
-const inviteManager = async (
-  campId: string,
-  email: string,
-  expiresAt?: string,
-) => {
-  return prisma.campManager.create({
-    data: {
-      camp: { connect: { id: campId } },
-      expiresAt,
-      invitation: {
-        create: {
+  async resolveManagerInvitations(email: string, userId: string) {
+    await prisma.campManager.updateMany({
+      where: {
+        invitation: {
           email,
         },
       },
-    },
-    include: {
-      invitation: true,
-      user: true,
-    },
-  });
-};
+      data: {
+        userId,
+      },
+    });
 
-const updateManagerById = async (id: string, expiresAt?: string | null) => {
-  return prisma.campManager.update({
-    where: {
-      id,
-    },
-    data: {
-      expiresAt,
-    },
-    include: {
-      invitation: true,
-      user: true,
-    },
-  });
-};
+    await prisma.invitation.deleteMany({
+      where: {
+        email,
+      },
+    });
+  }
 
-const removeManager = async (id: string) => {
-  return prisma.campManager.delete({
-    where: { id },
-  });
-};
+  async addManager(campId: string, userId: string, expiresAt?: string) {
+    return prisma.campManager.create({
+      data: {
+        campId,
+        userId,
+        expiresAt,
+      },
+      include: {
+        user: true,
+        invitation: true,
+      },
+    });
+  }
 
-export default {
-  getManagers,
-  getManagerByEmail,
-  getManagerById,
-  getManagerByUserId,
-  addManager,
-  inviteManager,
-  updateManagerById,
-  resolveManagerInvitations,
-  campManagerExistsWithUserIdAndCampId,
-  removeManager,
-};
+  async inviteManager(campId: string, email: string, expiresAt?: string) {
+    return prisma.campManager.create({
+      data: {
+        camp: { connect: { id: campId } },
+        expiresAt,
+        invitation: {
+          create: {
+            email,
+          },
+        },
+      },
+      include: {
+        invitation: true,
+        user: true,
+      },
+    });
+  }
+
+  async updateManagerById(id: string, expiresAt?: string | null) {
+    return prisma.campManager.update({
+      where: {
+        id,
+      },
+      data: {
+        expiresAt,
+      },
+      include: {
+        invitation: true,
+        user: true,
+      },
+    });
+  }
+
+  async removeManager(id: string) {
+    return prisma.campManager.delete({
+      where: { id },
+    });
+  }
+}
+
+export default new ManagerService();
