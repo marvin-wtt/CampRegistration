03e176b452be171ef4b14f47ef49b2b330499854
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-03-20 08:49:47 +0100

    fix: Incorrect import

--- backend/src/i18n/de-DE/index.ts
+++ backend/src/i18n/de-DE/index.ts
@@ -2,7 +2,7 @@ import auth from './auth/index.js';
 import camp from './camp/index.js';
 import email from './email/index.js';
 import manager from './manager/index.js';
-import registration from './manager/index.js';
+import registration from './registration/index.js';
 
 export default {
   // Translation is the default namespace
