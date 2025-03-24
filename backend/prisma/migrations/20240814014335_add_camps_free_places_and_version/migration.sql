5ade886bcb98ba3d79ab9dd93002a937cf38efea
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-03-24 11:09:26 +0100

    fix: Remove defaults from JSON column as it is not supported

--- backend/prisma/migrations/20240814014335_add_camps_free_places_and_version/migration.sql
+++ backend/prisma/migrations/20240814014335_add_camps_free_places_and_version/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Note:
-   The free_places column was first added so that all existing columns have a valid value.
-   It is revered in the following query as it should not have a default value.
-*/
-
 -- AlterTable
-ALTER TABLE `camps` ADD COLUMN `free_places` JSON NOT NULL DEFAULT 0,
+ALTER TABLE `camps` ADD COLUMN `free_places` JSON NOT NULL,
     ADD COLUMN `version` INTEGER UNSIGNED NOT NULL DEFAULT 1;
-
--- AlterTable
-ALTER TABLE `camps` ALTER COLUMN `free_places` DROP DEFAULT;
