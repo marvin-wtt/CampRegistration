2c995525b6f559e2daa54ed94ab87c865a739039
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-04-26 04:02:58 +0200

    fix: Drop role default

--- backend/prisma/migrations/20250425225622_add_camp_managers_role_column/migration.sql
+++ backend/prisma/migrations/20250425225622_add_camp_managers_role_column/migration.sql
@@ -1,2 +1,9 @@
+/*
+ A default is created first to ensure migration. The default is dropped right after
+*/
+
 -- AlterTable
 ALTER TABLE `camp_managers` ADD COLUMN `role` TINYTEXT NOT NULL DEFAULT 'DIRECTOR';
+
+-- AlterTable
+ALTER TABLE `camp_managers` ALTER COLUMN `role` DROP DEFAULT;
