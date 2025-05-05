8494fef35d91d5069e37846798946cdde1edf2d7
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-05-05 16:06:45 +0200

    fix: Invalid cron imports

--- backend/src/jobs/handler.ts
+++ backend/src/jobs/handler.ts
@@ -1,24 +1,23 @@
-import type Chron from 'croner';
+import type { Cron } from 'croner';
 import logger from '#core/logger';
-import type Cron from 'croner';
 import moment from 'moment';
 
-export const errorHandler = (error: unknown, job: Chron) => {
+export const errorHandler = (error: unknown, job: Cron) => {
   logger.error(`Job ${job.name ?? '??'} failed. ${JSON.stringify(error)}`);
 };
 
-export const protectionHandler = (job: Chron) => {
+export const protectionHandler = (job: Cron) => {
   const startTime = job.currentRun()?.toISOString();
   logger.warning(
     `Job ${job.name ?? '??'} was blocked by call started at ${startTime ?? '??'}`,
   );
 };
 
-export const terminationHandler = (reason: string, job: Chron) => {
+export const terminationHandler = (reason: string, job: Cron) => {
   logger.info(`Job ${job.name ?? '??'} terminated. ${reason}`);
 };
 
-export const executionHandler = (job: Chron) => {
+export const executionHandler = (job: Cron) => {
   logger.info(`Job ${job.name ?? '??'} executing...`);
 };
 
