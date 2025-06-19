7ad053dbb58bdea42d029dda2af6fd98979843ee
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-06-19 22:25:09 +0200

    feat: Add auth e2e tests

--- e2e/cypress/e2e/authentication.cy.ts
+++ e2e/cypress/e2e/authentication.cy.ts
@@ -109,15 +109,79 @@ describe("Authentication", () => {
       cy.wait("@registerUser").its("response.statusCode").should("eq", 201);
       cy.location("pathname").should("eq", "/login");
 
-      cy.log("Verifying email sent to user");
+      cy.log("Verifying user cannot log in before email confirmation");
+      cy.getBySel("login-form").getBySel("email").type("test@example.com");
+      cy.getBySel("login-form").getBySel("password").type("Password123#");
+      cy.getBySel("login-form").getBySel("submit").click();
 
+      cy.location("pathname").should("eq", "/verify-email");
+
+      cy.log("Verifying email sent to user");
       cy.getEmails("test@example.com").then((emails) => {
         expect(emails).to.have.length(1);
         const email = emails[0];
 
-        expect(email.headers.subject).to.include("Welcome to Our Service");
-        expect(email.body).to.include("Thank you for registering, Test User!");
+        cy.document().document().invoke("write", email.html);
       });
+
+      cy.get(".confirm-email-link a")
+        .first()
+        .invoke("removeAttr", "target")
+        .click();
+      cy.location("pathname").should("eq", "/login");
+
+      cy.log("Verifying user can log in after email confirmation");
+      const loginForm = cy.getBySel("login-form");
+      loginForm.getBySel("password").type("Password123#");
+      loginForm.getBySel("submit").click();
+
+      cy.location("pathname").should("eq", "/management");
+    });
+  });
+
+  describe("Password Reset", () => {
+    it("should request password reset", () => {
+      cy.intercept("POST", `/api/v1/auth/forgot-password`).as("forgotPassword");
+      cy.intercept("POST", `/api/v1/auth/reset-password`).as("resetPassword");
+      cy.intercept("POST", `/api/v1/auth/login`).as("loginUser");
+
+      cy.visit("/forgot-password");
+
+      const forgotPasswordForm = cy.getBySel("forgot-password-form");
+      forgotPasswordForm.getBySel("email").type("john@example.com");
+      forgotPasswordForm.getBySel("submit").type("john@example.com");
+
+      cy.wait("@forgotPassword").its("response.statusCode").should("eq", 204);
+
+      cy.getEmails("john@example.com").then((emails) => {
+        expect(emails).to.have.length(1);
+        const email = emails[0];
+
+        cy.document().document().invoke("write", email.html);
+      });
+
+      cy.get(".reset-email-link a")
+        .first()
+        .invoke("removeAttr", "target")
+        .click();
+      cy.location("pathname").should("eq", "/reset-password");
+
+      const resetPasswordForm = cy.getBySel("reset-password-form");
+      resetPasswordForm.getBySel("password").type("NewPassword123#");
+      resetPasswordForm.getBySel("confirm-password").type("NewPassword123#");
+      resetPasswordForm.getBySel("submit").click();
+
+      cy.wait("@resetPassword").its("response.statusCode").should("eq", 204);
+
+      cy.location("pathname").should("eq", "/login");
+
+      const loginForm = cy.getBySel("login-form");
+      loginForm.getBySel("email").type("john@example.com");
+      loginForm.getBySel("password").type("NewPassword123#");
+      loginForm.getBySel("submit").click();
+
+      cy.wait("@loginUser").its("response.statusCode").should("eq", 200);
+      cy.location("pathname").should("eq", "/management");
     });
   });
 });
