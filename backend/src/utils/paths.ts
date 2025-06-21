e864bf3fe546e11ef37f694ae7942f383f761089
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-06-21 22:31:16 +0200

    fix: Remove circular dependency

--- backend/src/utils/paths.ts
+++ backend/src/utils/paths.ts
@@ -1,5 +1,4 @@
 import { packageDirectorySync } from 'pkg-dir';
-import logger from '#core/logger';
 
 const dirname = import.meta.dirname;
 
@@ -10,8 +9,6 @@ const packageDir = packageDirectorySync({
 if (!packageDir) {
   throw new Error('Could not determine the root path of the package.');
 }
-
-logger.info(`App root path is: ${packageDir}`);
 
 // Ensure that the type is string only
 const projectRoot = packageDir;
