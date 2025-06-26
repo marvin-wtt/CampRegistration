29465f507464a4e2da803d3167eb87b541e51ca5
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-06-27 00:19:00 +0200

    Revert "fix: Katch test name with action"
    This reverts commit 1b0734acde45fc0b54a43e1f9a9acf6b71bb465e.

--- backend/tests/integration/maintenance.test.ts
+++ backend/tests/integration/maintenance.test.ts
@@ -10,19 +10,19 @@ describe('maintenance', () => {
   afterAll(resetEmvVariables);
 
   it('should respond with `200` status code when maintenance mode is disabled', async () => {
-    process.env.MAINTENANCE_MODE = 'false';
-
-    await restartApp();
-
-    await request().get(`/api/v1/health`).expect(200);
-  });
-
-  it('should respond with `503` status code when maintenance mode is enabled', async () => {
     process.env.MAINTENANCE_MODE = 'true';
 
     await restartApp();
 
     await request().get(`/api/v1/health`).expect(503);
+  });
+
+  it('should respond with `503` status code when maintenance mode is enabled', async () => {
+    process.env.MAINTENANCE_MODE = 'false';
+
+    await restartApp();
+
+    await request().get(`/api/v1/health`).expect(200);
   });
 
   it('should respond with `200` status code when maintenance mode is not set', async () => {
