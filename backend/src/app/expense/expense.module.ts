82f717680c7c14948a7a129899abec7239532731
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-06-28 04:32:11 +0200

    feat: Move expense file guard to a separate file

--- backend/src/app/expense/expense.module.ts
+++ backend/src/app/expense/expense.module.ts
@@ -4,16 +4,13 @@ import type {
   RoleToPermissions,
 } from '#core/base/AppModule';
 import type { ExpensePermission } from '@camp-registration/common/permissions';
-import expenseService from '#app/expense/expense.service';
 import { ExpenseRouter } from '#app/expense/expense.routes';
-import campService from '#app/camp/camp.service';
-import { campManager, type GuardFn } from '#guards/index';
-import type { Request } from 'express';
-import { registerFileGuard } from '#app/file/file.guard.js';
+import { registerFileGuard } from '#app/file/file.guard';
+import { expenseFileGuard } from '#app/expense/expense.guard';
 
 export class ExpenseModule implements AppModule {
   registerRoutes(router: AppRouter) {
-    registerFileGuard('expense', this.fileGuard);
+    registerFileGuard('expense', expenseFileGuard);
 
     router.useRouter('/camps/:campId/expenses', new ExpenseRouter());
   }
@@ -36,25 +33,6 @@ export class ExpenseModule implements AppModule {
       VIEWER: [],
     };
   }
-
-  private fileGuard = async (req: Request): Promise<GuardFn> => {
-    const file = req.modelOrFail('file');
-
-    if (!file.expenseId) {
-      throw new Error('Invalid guard handler: expenseId not found in file');
-    }
-
-    const expense = await expenseService.getExpenseWithCampById(file.expenseId);
-    if (!expense) {
-      throw new Error('Expense related to file not found');
-    }
-    const camp = await campService.getCampById(expense.camp.id);
-
-    req.setModelOrFail('expense', expense);
-    req.setModelOrFail('camp', camp);
-
-    return campManager('camp.expenses.view');
-  };
 }
 
 export default new ExpenseModule();
