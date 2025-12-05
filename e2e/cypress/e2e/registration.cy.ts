33e6f2b565c2482491a1748678ff37d26461149c
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-12-06 00:49:12 +0100

    fix: Refactor registration form submission button selector

--- e2e/cypress/e2e/registration.cy.ts
+++ e2e/cypress/e2e/registration.cy.ts
@@ -34,7 +34,6 @@ describe("registration page", () => {
     // Submit
     cy.getBySel("registration-form")
       .find(".sd-navigation__complete-btn")
-      .find("input")
       .click();
 
     cy.wait("@createCampRegistration").then((interception) => {
@@ -84,8 +83,7 @@ describe("registration page", () => {
 
     // Submit
     cy.getBySel("registration-form")
-      .find("#sv-nav-complete")
-      .find("input")
+      .find(".sd-navigation__complete-btn")
       .click();
 
     cy.wait("@createCampRegistration").then((interception) => {
