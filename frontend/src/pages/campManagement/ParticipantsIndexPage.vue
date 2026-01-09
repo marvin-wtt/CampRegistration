0eed258699334a62cbe75dc364ce6058bc817a1c
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-01-09 01:33:50 +0100

    fix: Remove comment

--- frontend/src/pages/campManagement/ParticipantsIndexPage.vue
+++ frontend/src/pages/campManagement/ParticipantsIndexPage.vue
@@ -79,10 +79,8 @@ async function onTemplatesPrint(templateIds: string[]) {
   }
 
   const templates = templateStore.data
-    ?.filter((t) => templateIds.includes(t.id))
+    .filter((t) => templateIds.includes(t.id))
     .sort((a, b) => (a.order ?? 999) - (b.order ?? 999));
-
-  // Ensure data is present
 
   // Build payload
   const payload: PrintTablesPayload = {
