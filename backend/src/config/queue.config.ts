4edf9dff40fe1376d66c29e0b908084624b5eae1
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-01-17 05:07:43 +0100

    feat: Merge config to single file

--- backend/src/config/queue.config.ts
+++
@@ -1,5 +0,0 @@
-import { env } from '#config/enviroment';
-
-export default {
-  driver: env.QUEUE_DRIVER,
-};
--- backend/src/config/redis.config.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '#config/enviroment';
-
-export default {
-  host: env.REDIS_HOST,
-  port: env.REDIS_PORT,
-  password: env.REDIS_PASSWORD,
-  db: env.REDIS_DB,
-  username: env.REDIS_USERNAME,
-};
