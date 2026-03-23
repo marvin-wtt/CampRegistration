e38ec0847ae4617799ee830018b18c1ed5c40c8a
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-03-23 01:12:38 +0100

    fix: Send event message to unique emails only once

--- backend/vitest.config.integration.ts
+++ backend/vitest.config.integration.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [tsconfigPaths({ projects: ['./tsconfig.test.json'] })],
   test: {
-    include: ['tests/integration/**/registration.test.ts'],
+    include: ['tests/integration/**/*.test.ts'],
     maxWorkers: 1,
     setupFiles: ['tests/integration/setup.ts'],
     coverage: {
