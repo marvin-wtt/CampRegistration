a1eb71a7e2d5270f9e9affbdc92f6fbbf3e9a18e
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-03-31 03:46:37 +0200

    fix: remove prisma env helper as the env variable is not present in sone CI jobs

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -7,6 +7,7 @@ export default defineConfig({
     seed: 'tsx prisma/seeder/index.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    // Do not use the env helper function as the URL is not guaranteed to be set in CI environments
+    url: process.env.DATABASE_URL ?? '',
   },
 });
