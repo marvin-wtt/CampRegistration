f23f480f5b2861ada4f339201d97f1e46027b4e2
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-04-01 14:23:02 +0200

    fix: Update rate limiting for ipv6 bypass

--- backend/src/middlewares/rateLimiter.middleware.ts
+++ backend/src/middlewares/rateLimiter.middleware.ts
@@ -1,22 +1,31 @@
-import { rateLimit } from 'express-rate-limit';
+import { rateLimit, ipKeyGenerator } from 'express-rate-limit';
+import type { Request } from 'express';
+
+const keyGenerator = (name: string) => {
+  return (req: Request): string => {
+    const key = ipKeyGenerator(req.ip ?? '');
+
+    return `${name}:${key}`;
+  };
+};
 
 export const authLimiter = rateLimit({
   windowMs: 60 * 1000,
   limit: 15,
   skipSuccessfulRequests: true,
-  keyGenerator: (request) => `Auth:${request.ip ?? ''}`,
+  keyGenerator: keyGenerator('Auth'),
 });
 
 export const generalLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
   limit: 250, // limit each IP to 250 requests per windowMs
-  keyGenerator: (request) => `General:${request.ip ?? ''}`,
+  keyGenerator: keyGenerator('General'),
 });
 
 export const staticLimiter = rateLimit({
   windowMs: 60 * 1000,
   limit: 500,
-  keyGenerator: (request) => `Static:${request.ip ?? ''}`,
+  keyGenerator: keyGenerator('Static'),
 });
 
 export default {
