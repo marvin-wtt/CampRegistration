a34a33a5c53e33e9d118b8eb1a8c05d16c54325d
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-05-19 03:02:08 +0200

    fix: Add missing translations

---
+++ frontend/src/i18n/cs-CZ/stores/program-planner.ts
@@ -0,0 +1,11 @@
+export default {
+  create: {
+    error: 'Chyba při vytváření události',
+  },
+  update: {
+    error: 'Chyba při aktualizaci události',
+  },
+  delete: {
+    error: 'Chyba při odstraňování události',
+  },
+};
---
+++ frontend/src/i18n/pl-PL/stores/program-planner.ts
@@ -0,0 +1,11 @@
+export default {
+  create: {
+    error: 'Błąd podczas tworzenia wydarzenia',
+  },
+  update: {
+    error: 'Błąd podczas aktualizowania wydarzenia',
+  },
+  delete: {
+    error: 'Błąd podczas usuwania wydarzenia',
+  },
+};
