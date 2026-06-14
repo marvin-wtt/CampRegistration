8ad81634bfc156e58a5fdd61ed70cb94c4a6028b
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-06-15 00:17:11 +0200

    fix: update share button logic to use `disable` instead of `v-if` and add tooltips for improved UX

--- frontend/src/components/campManagement/index/ResultsItemMenu.vue
+++ frontend/src/components/campManagement/index/ResultsItemMenu.vue
@@ -2,8 +2,8 @@
   <q-menu>
     <q-list style="min-width: 100px">
       <q-item
-        v-if="active"
         v-close-popup
+        :disable="!active"
         clickable
         @click="emit('share')"
       >
@@ -15,6 +15,9 @@
             {{ t('action.share') }}
           </q-item-label>
         </q-item-section>
+        <q-tooltip v-if="!active">
+          {{ t('tooltip.share_disabled') }}
+        </q-tooltip>
       </q-item>
 
       <q-item
@@ -34,7 +37,7 @@
       </q-item>
 
       <q-item
-        v-if="!active && can('camp.delete')"
+        v-if="can('camp.delete')"
         v-close-popup
         class="text-negative"
         clickable
@@ -86,6 +89,8 @@ action:
   enable: 'Enable'
   share: 'Share'
   disable: 'Disable'
+tooltip:
+  share_disabled: 'Sharing is only available while registration is open'
 </i18n>
 
 <i18n lang="yaml" locale="de">
@@ -96,6 +101,8 @@ action:
   enable: 'Aktivieren'
   share: 'Teilen'
   disable: 'Deaktivieren'
+tooltip:
+  share_disabled: 'Teilen ist nur möglich, während die Anmeldung geöffnet ist'
 </i18n>
 
 <i18n lang="yaml" locale="fr">
@@ -106,6 +113,8 @@ action:
   enable: 'Activer'
   share: 'Partager'
   disable: 'Désactiver'
+tooltip:
+  share_disabled: "Le partage n'est disponible que lorsque les inscriptions sont ouvertes"
 </i18n>
 
 <i18n lang="yaml" locale="pl">
@@ -116,6 +125,8 @@ action:
   enable: 'Aktywuj'
   share: 'Udostępnij'
   disable: 'Dezaktywuj'
+tooltip:
+  share_disabled: 'Udostępnianie jest dostępne tylko podczas otwartej rejestracji'
 </i18n>
 
 <i18n lang="yaml" locale="cs">
@@ -126,4 +137,6 @@ action:
   enable: 'Aktivovat'
   share: 'Sdílet'
   disable: 'Deaktivovat'
+tooltip:
+  share_disabled: 'Sdílení je dostupné pouze během otevřené registrace'
 </i18n>
