2fe87d1ed1d8350d2872101cc4e5f24b68404953
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-06-27 02:50:58 +0200

    feat(messageDelivery): add bindContainers method to support DI configuration

--- backend/src/app/messageDelivery/message-delivery.module.ts
+++ backend/src/app/messageDelivery/message-delivery.module.ts
@@ -1,8 +1,12 @@
-import type { AppModule } from '#core/base/AppModule';
+import type { AppModule, BindOptions } from '#core/base/AppModule';
 import { registerFileGuard } from '#app/file/file.guard';
 import { messageDeliveryFileGuard } from '#app/messageDelivery/message-delivery.guard';
 
 export class MessageDeliveryModule implements AppModule {
+  bindContainers(options: BindOptions) {
+    options.bind(MessageDeliveryModule).toSelf().inSingletonScope();
+  }
+
   registerRoutes() {
     registerFileGuard('messageDelivery', {
       view: messageDeliveryFileGuard,
